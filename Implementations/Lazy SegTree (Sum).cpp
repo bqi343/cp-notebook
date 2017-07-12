65d186ac1cee43e9c8842d2828421352741453d1
Benjamin Qi <bqi343@gmail.com>
2017-07-12 10:41:24 -0400

    Update Lazy SegTree (Sum).cpp

--- Implementations/Lazy SegTree (Sum).cpp
+++ Implementations/Lazy SegTree (Sum).cpp
@@ -16,36 +16,40 @@ struct LazySegTree {
 	}
 	
 	void push(int ind, int L, int R) {
-	    sum[ind] += (R-L+1)*lazy[ind];
-	    if (L != R) lazy[2*ind] += lazy[ind], lazy[2*ind+1] += lazy[ind];
-	    lazy[ind] = 0;
+		sum[ind] += (R-L+1)*lazy[ind];
+	    	if (L != R) lazy[2*ind] += lazy[ind], lazy[2*ind+1] += lazy[ind];
+	    	lazy[ind] = 0;
 	}
-	 
+	
+	void build() {
+		for (int i = SZ-1; i > 0; --i) sum[i] = sum[2*i]+sum[2*i+1];
+	}
+	
 	void pull(int ind) {
-	    sum[ind] = sum[2*ind]+sum[2*ind+1];
+	    	sum[ind] = sum[2*ind]+sum[2*ind+1];
 	}
 	 
 	ll qsum(int lo, int hi, int ind = 1, int L = 0, int R = SZ-1) {
 		push(ind,L,R);
-	    if (lo > R || L > hi) return 0;
-	    if (lo <= L && R <= hi) return sum[ind];
+	    	if (lo > R || L > hi) return 0;
+	    	if (lo <= L && R <= hi) return sum[ind];
 	    
-	    int M = (L+R)/2;
-	    return qsum(lo,hi,2*ind,L,M)+qsum(lo,hi,2*ind+1,M+1,R);
+	    	int M = (L+R)/2;
+	    	return qsum(lo,hi,2*ind,L,M)+qsum(lo,hi,2*ind+1,M+1,R);
 	}
 	 
 	void upd(int lo, int hi, int inc, int ind = 1, int L = 0, int R = SZ-1) { // take account for minimum as well
-	    push(ind,L,R);
-	    if (hi < L || R < lo) return;
-	    if (lo <= L && R <= hi) {
-	        lazy[ind] = inc;
-	        push(ind,L,R);
-	        return;
-	    }
+	    	push(ind,L,R);
+	    	if (hi < L || R < lo) return;
+	    	if (lo <= L && R <= hi) {
+	    	    	lazy[ind] = inc;
+	   		push(ind,L,R);
+	  	      	return;
+	   	}
 	    
-	    int M = (L+R)/2;
-	    upd(lo,hi,inc,2*ind,L,M); upd(lo,hi,inc,2*ind+1,M+1,R);
-	    pull(ind);
+	    	int M = (L+R)/2;
+	    	upd(lo,hi,inc,2*ind,L,M); upd(lo,hi,inc,2*ind+1,M+1,R);
+	    	pull(ind);
 	}
 };
  

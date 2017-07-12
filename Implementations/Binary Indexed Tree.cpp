97a8205ab077a06bb98f3aad51362627b5c63377
Benjamin Qi <bqi343@gmail.com>
2017-07-12 10:28:34 -0400

    Update Binary Indexed Tree.cpp

--- Implementations/Binary Indexed Tree.cpp
+++ Implementations/Binary Indexed Tree.cpp
@@ -4,21 +4,32 @@ using namespace std;
  
 const int SZ = 100000;
  
-int bit[SZ+1];
- 
-void update(int k, int val) {
-	for( ;k <= SZ; k += (k & -k)) bit[k] += val;
-}
- 
-int query(int k) {
-	int temp = 0;
-	for (;k>0;k -= (k & -k)) temp += bit[k];
-	return temp;
-}
+struct BinaryIndexedTree {
+	int bit[SZ+1];
+	
+	BinaryIndexedTree() {
+		memset(bit,0,sizeof bit);
+	}
+	
+	void upd(int k, int val) {
+		for( ;k <= SZ; k += (k&-k)) bit[k] += val;
+	}
+	 
+	int query(int k) {
+		int temp = 0;
+		for (;k > 0;k -= (k&-k)) temp += bit[k];
+		return temp;
+	}
+	
+	int query(int l, int r) {
+		return query(r)-query(l-1);
+	}
+};
  
 int main() {
-	update(5,2);
-	update(4,1);
-	cout << query(5)-query(3);
+	BinaryIndexedTree b;
+	b.upd(5,2);
+	b.upd(4,1);
+	cout << b.query(3,5);
 }
  

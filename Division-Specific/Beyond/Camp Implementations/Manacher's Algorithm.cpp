088dd4dd0951eae3010f2263a78dab8ed3ed1f17
Benjamin Qi <bqi343@gmail.com>
2017-08-07 10:06:25 -0400

    Update Manacher's Algorithm.cpp

--- Division-Specific/Beyond/Camp Implementations/Manacher's Algorithm.cpp
+++ Division-Specific/Beyond/Camp Implementations/Manacher's Algorithm.cpp
@@ -26,22 +26,25 @@ const int MOD = 1000000007;
 double PI = 4*atan(1);
 
 vi pal_array(string s) {
-    int n = s.length();
-    s = "@" + s + "$";
-    vi len(n + 1);
+	int n = s.length();
+	s = "@" + s + "$";
+	vi len(n + 1);
+	
+	int l = 1, r = 1;
+    	FOR(i,1,n+1) {
+        	len[i] = min(r - i, len[l + (r - i)]);
+        	while(s[i - len[i]] == s[i + len[i]]) len[i]++;
+        	if(i + len[i] > r) l = i - len[i], r = i + len[i];
+    	}
     
-    int l = 1, r = 1;
-    FOR(i,1,n+1) {
-        len[i] = min(r - i, len[l + (r - i)]);
-        while(s[i - len[i]] == s[i + len[i]]) len[i]++;
-        if(i + len[i] > r) l = i - len[i], r = i + len[i];
-    }
-    
-    len.erase(len.begin());
-    return len;
+    	len.erase(len.begin());
+    	return len;
 }
 
 int main() {
 	vi x = pal_array("abacaba");
 	for (int i: x) cout << i << " ";
 }
+
+// Output 1 2 1 4 1 2 1
+// Insert #'s between characters for even-length palindromes

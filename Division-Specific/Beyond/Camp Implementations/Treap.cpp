855f61e4d0db887f4ddfcb80a3e85a5a998847b7
Benjamin Qi <bqi343@gmail.com>
2017-08-21 15:47:41 -0400

    Update Treap.cpp

--- Division-Specific/Beyond/Camp Implementations/Treap.cpp
+++ Division-Specific/Beyond/Camp Implementations/Treap.cpp
@@ -31,7 +31,7 @@ class node {
         }
 };
 
-class treap{
+class treap {
     public:
         node *root, *null;
     
@@ -43,12 +43,10 @@ class treap{
         
         void update(node*& p){ 
             if(p == null) return;
-            p->sz = 1;
-            
-            F0R(t,2) p->sz += p->c[t]->sz;
+            p->sz = 1+p->c[0]->sz+p->c[1]->sz;
         }
         
-        void rot (node *&p, int d){ 
+        void rot (node *&p, int d) { // takes care of both left / right rotations
             node *q = p->c[d]; 
             p->c[d] = q->c[d^1]; 
             q->c[d^1] = p; 
@@ -58,8 +56,7 @@ class treap{
         
         void ins(node *&p, int x){
             if (p == null) {
-                p = new node(x); 
-                p->c[0] = p->c[1] = null;
+                p = new node(x);  p->c[0] = p->c[1] = null;
             } else {
                 int t = (x < p->val) ? 0 : 1;
                 ins(p->c[t], x); 
@@ -75,13 +72,12 @@ class treap{
                 int t = (x < p->val) ? 0 : 1;
                 del(p->c[t],x); 
             }
-            if (p) update(p);
+            update(p);
         }
         
         void del(node *&p){ 
             if (p->c[0] == null && p->c[1] == null) {
-                delete p;
-                p = null;
+                delete p; p = null;
                 return;
             }
             int t = (p->c[0]->pri > p->c[1]->pri) ? 0 : 1;
@@ -113,7 +109,8 @@ class treap{
         }
         
         bool find(int x){ return find(root,x); }
-        int get(int x) { return get(root,x);}
+ 
+        int get(int x) { return get(root,x); }
 }; 
 
 treap T;

49c58a6b40b0c2d7d425e1ebd83c65d0db5c14f2
Benjamin Qi <bqi343@gmail.com>
2017-08-28 14:38:47 -0400

    asdf

--- Implementations/Range Trees/Segment Tree.cpp
+++ Implementations/Range Trees/Segment Tree.cpp
@@ -1,9 +1,10 @@
-// http://codeforces.com/blog/entry/18051
-#include <bits/extc++.h>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
 using namespace __gnu_pbds;
- 
+
 typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int, int> pii;
@@ -23,44 +24,63 @@ template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_ord
 #define ub upper_bound
 
 const int MOD = 1000000007;
+const ll INF = 1e18;
 
-template<int SZ>
-class Seg {
-	public:
-		typedef int T;
-		T seg[2*SZ], MN = 0;
-
-		Seg() {
-			memset(seg,0,sizeof seg);	
-		}
-
-		T comb(T a, T b) { return a+b; }
-
-		void upd(int p, T value) {  // set value at position p
-			for (seg[p += SZ] = value; p > 1; p >>= 1) seg[p>>1] = comb(seg[p],seg[p^1]);
-		}
-		
-		void build() {
-			for (int i = SZ-1; i > 0; --i) seg[i] = comb(seg[2*i],seg[2*i+1]);	
-		}
-
-		T query(int l, int r) {  // nterval [l, r)
-			T res = MN;
-			for (l += SZ, r += SZ; l < r; l >>= 1, r >>= 1) {
-				if (l&1) res = comb(res,seg[l++]);
-				if (r&1) res = comb(res,seg[--r]);
-			}
-			return res;
-		}
+template<int SZ> struct Seg {
+    typedef int T;
+    T seg[2*SZ], MN = 0;
+    
+    Seg() {
+        memset(seg,0,sizeof seg);
+    }
+    
+    T comb(T a, T b) { return a+b; } // easily change this to min or max
+    
+    void upd(int p, T value) {  // set value at position p
+        for (seg[p += SZ] = value; p > 1; p >>= 1) seg[p>>1] = comb(seg[p],seg[p^1]);
+    }
+    
+    void build() {
+        F0Rd(i,SZ) seg[i] = comb(seg[2*i],seg[2*i+1]);
+    }
+    
+    T query(int l, int r) {  // sum on interval [l, r]
+        T res = MN; r++;
+        for (l += SZ, r += SZ; l < r; l >>= 1, r >>= 1) {
+            if (l&1) res = comb(res,seg[l++]);
+            if (r&1) res = comb(res,seg[--r]);
+        }
+        return res;
+    }
 };
 
+template<int SZ> struct BIT {
+    int bit[SZ+1];
+    
+    BIT() {
+        memset(bit,0,sizeof bit);
+    }
+    
+    void upd(int k, int val) {
+        for( ;k <= SZ; k += (k&-k)) bit[k] += val;
+    }
+    
+    int query(int k) {
+        int temp = 0;
+        for (;k > 0;k -= (k&-k)) temp += bit[k];
+        return temp;
+    }
+    int query(int l, int r) { return query(r)-query(l-1); }
+};
 
 int main() {
-	Seg<1<<17> a;
-	a.upd(0,10);
-	a.upd(2,5);
-	cout << a.query(0,3); // 15
+    BIT<1<<17> b;
+    b.upd(5,2);
+    b.upd(4,1);
+    cout << b.query(3,5) << "\n"; // 3
+    
+    Seg<1<<17> s;
+    s.upd(5,2);
+    s.upd(4,1);
+    cout << s.query(3,5) << "\n"; // 3
 }
-
-// read!
-// ll vs. int!

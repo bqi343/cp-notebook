8fe190a3289d864fb135de0a0655d75a4398469c
Benjamin Qi <bqi343@gmail.com>
2017-10-17 08:46:44 -0400

    Update (Camp) Biconnected Components.cpp

--- List of Topics/(2+) Graphs/Implementations/Connected Components/(Camp) Biconnected Components.cpp
+++ List of Topics/(2+) Graphs/Implementations/Connected Components/(Camp) Biconnected Components.cpp
@@ -27,117 +27,77 @@ template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_ord
 
 const int MOD = 1000000007;
 
-const int NIL = -1;
-
-int count = 0;
-
-struct Edge {
-    int u, v;
-    Edge(int U, int V) { u = U, v = V; }
-};
-
-struct Graph {
-    int V, E;    
-    vector<vi> adj, fin;
-    vi disc,low,parent;
-    vector<Edge> st;
-   
-    Graph(int v) {
-        V = v, E = 0;
-        adj.resize(V);
+struct BCC {
+    int V, ti = 0;
+    vector<vi> adj;
+    vi par, disc, low;
+    vector<vector<pii>> fin;
+    vector<pii> st;
+    
+    void init(int _V) {
+        V = _V;
+        par.resize(V), disc.resize(V), low.resize(V), adj.resize(V);
+        F0R(i,V) par[i] = disc[i] = low[i] = -1;
     }
-    void addEdge(int v, int w) {
-        adj[v].pb(w), E++;
+    void addEdge(int u, int v) {
+        adj[u].pb(v), adj[v].pb(u);
     }
     
-    // st -- >> To store visited edges
-
-    void BCCUtil(int u) {
-        static int time = 0;
-       
-        disc[u] = low[u] = ++time;
-        int children = 0;
-       
-        for (int v: adj[u]) {
-            if (disc[v] == -1) {
-                children++;
-                parent[v] = u;
-                st.pb(Edge(u,v));
-                BCCUtil(v);
-       
-                // Check if the subtree rooted with 'v' has a
-                // connection to one of the ancestors of 'u'
-                // Case 1 -- per Strongly Connected Components Article
-                low[u]  = min(low[u], low[v]);
-      
-                //If u is an articulation point,
-                //pop all edges from stack till u -- v
-                if( (disc[u] == 1 && children > 1) || (disc[u] > 1 && low[v] >= disc[u]) ) {
-                    vi z;
-                    while(st.back().u != u || st.back().v != v) {
-                        z.pb(st.back().u); z.pb(st.back().v);
-                        st.pop_back();
-                    }
-                    z.pb(st.back().u); z.pb(st.back().v);
-                    sort(z.begin(),z.end()); 
-                    z.erase(unique(z.begin(),z.end()),z.end());
-                    fin.pb(z);
-                    st.pop_back();
+    void BCCutil(int u) {
+        disc[u] = low[u] = ti++;
+        int child = 0;
+        
+        for (int i: adj[u]) if (i != par[u]) {
+            if (disc[i] == -1) {
+                par[i] = u;
+                child ++;
+                st.pb({u,i});
+                BCCutil(i);
+                low[u] = min(low[u],low[i]);
+                
+                if ((disc[u] == 0 && child > 1) || (disc[u] != 0 && disc[u] <= low[i])) {
+                    vector<pii> tmp;
+                    while (st.back() != mp(u,i)) tmp.pb(st.back()), st.pop_back();
+                    tmp.pb(st.back()), st.pop_back();
+                    fin.pb(tmp);
                 }
-            }
-       
-            // Update low value of 'u' only of 'v' is still in stack
-            // (i.e. it's a back edge, not cross edge).
-            // Case 2 -- per Strongly Connected Components Article
-            else if(v != parent[u] && disc[v] < low[u]) {
-                low[u]  = min(low[u], disc[v]);
-                st.pb(Edge(u,v));
+            } else if (disc[i] < low[u]) {
+                low[u] = disc[i];
+                st.pb({u,i});
             }
         }
     }
     
-    void BCC() {
-        disc.resize(V), low.resize(V), parent.resize(V);
-        F0R(i,V) disc[i] = low[i] = parent[i] = NIL;
-       
-        F0R(i,V) {
-            if (disc[i] == NIL) BCCUtil(i);
-              
-            vi z;
-            while(st.size() > 0) {
-                z.pb(st.back().u), z.pb(st.back().v);
-                st.pop_back();
-            }
-            if (z.size()) {
-                sort(z.begin(),z.end());
-                z.erase(unique(z.begin(),z.end()),z.end());
-                fin.pb(z);
-            }
+    void bcc() {
+        F0R(i,V) if (disc[i] == -1) {
+            BCCutil(i);
+            if (st.size()) fin.pb(st);
+            st.clear();
         }
     }
 };
 
 int main() {
-    ios_base::sync_with_stdio(0);cin.tie(0);
-	Graph g(12);
-    g.addEdge(0,1);g.addEdge(1,0);
-    g.addEdge(1,2);g.addEdge(2,1);
-    g.addEdge(1,3);g.addEdge(3,1);
-    g.addEdge(2,3);g.addEdge(3,2);
-    g.addEdge(2,4);g.addEdge(4,2);
-    g.addEdge(3,4);g.addEdge(4,3);
-    g.addEdge(1,5);g.addEdge(5,1);
-    g.addEdge(0,6);g.addEdge(6,0);
-    g.addEdge(5,6);g.addEdge(6,5);
-    g.addEdge(5,7);g.addEdge(7,5);
-    g.addEdge(5,8);g.addEdge(8,5);
-    g.addEdge(7,8);g.addEdge(8,7);
-    g.addEdge(8,9);g.addEdge(9,8);
-    g.addEdge(10,11);g.addEdge(11,10);
-    g.BCC();
+	ios_base::sync_with_stdio(0);cin.tie(0);
+	BCC g; g.init(12);
+	g.addEdge(0,1);
+    g.addEdge(1,2);
+    g.addEdge(1,3);
+    g.addEdge(2,3);
+    g.addEdge(2,4);
+    g.addEdge(3,4);
+    g.addEdge(1,5);
+    g.addEdge(0,6);
+    g.addEdge(5,6);
+    g.addEdge(5,7);
+    g.addEdge(5,8);
+    g.addEdge(7,8);
+    g.addEdge(8,9);
+    g.addEdge(10,11);
+    g.bcc();
     
     for (auto a: g.fin) {
-        for (int b: a) cout << b << " ";
+        for (pii b: a) cout << b.f << " " << b.s << " | ";
         cout << "\n";
     }
 }

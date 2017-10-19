479e9c1150f2ef7f8310c7636863e5895e2aa01c
Benjamin Qi <bqi343@gmail.com>
2017-10-19 13:34:09 -0400

    Update (Plat) 2D SegTree : SegBit.cpp

--- List of Topics/(2+) Data Structures/Implementations/2D Range Queries/(Plat) 2D SegTree : SegBit.cpp
+++ List of Topics/(2+) Data Structures/Implementations/2D Range Queries/(Plat) 2D SegTree : SegBit.cpp
@@ -1,35 +1,10 @@
-// 2D Point Update, Range Query
-// 9 min
+// 2D Segment Tree
+// Supports 2D Point Update, Range Query
+// Implementation time: 9 min
 
-#include <bits/stdc++.h>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/assoc_container.hpp>
-
-using namespace std;
-using namespace __gnu_pbds;
-
-typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int, int> pii;
-template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
-
-#define FOR(i, a, b) for (int i=a; i<(b); i++)
-#define F0R(i, a) for (int i=0; i<(a); i++)
-#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
-#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
-
-#define sz(x) (int)(x).size()
-#define mp make_pair
-#define pb push_back
-#define f first
-#define s second
-#define lb lower_bound
-#define ub upper_bound
-
-const int MOD = 1000000007;
 const int SZ = 1<<17;
 
-// Sparse SegTree
+// Sparse 1D SegTree
 struct node {
     int val = 0;
     node* c[2];
@@ -62,7 +37,7 @@ struct node {
     }
 };
 
-// 2D SegTree, note similarity to 1D SegTree!
+// 2D SegTree, sparse segtree of sparse 1D segtrees
 struct Node {
     node seg;
     Node* c[2];
@@ -93,6 +68,7 @@ struct Node {
 };
 
 // SegTree + BIT
+// Array of Sparse Segtrees
 struct SegBit {
     node seg[SZ+1];
     

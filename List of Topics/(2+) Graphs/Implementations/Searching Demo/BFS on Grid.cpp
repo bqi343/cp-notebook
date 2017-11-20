56d833a605d3c8de6e88fb83f5c30233f9d0293d
Benjamin Qi <bqi343@gmail.com>
2017-11-19 20:21:13 -0500

    Update BFS on Grid.cpp

--- List of Topics/(2+) Graphs/Implementations/Searching Demo/BFS on Grid.cpp
+++ List of Topics/(2+) Graphs/Implementations/Searching Demo/BFS on Grid.cpp
@@ -30,12 +30,12 @@ int dist[21][21];
 queue<pii> todo;
 
 void process(pii x) {
-    F0R(i,4) {
+	F0R(i,4) { // easily iterate through adjacent points
         pii y = {x.f+xdir[i],x.s+ydir[i]};
-        if (y.f < 0 || y.f > 20 || y.s < 0 || y.s > 20) continue;
-        if (dist[y.f][y.s] == MOD) {
+		if (y.f < 0 || y.f > 20 || y.s < 0 || y.s > 20) continue; // ignore this point if it's outside of grid
+        if (dist[y.f][y.s] == MOD) { // test whether point has been visited or not
             dist[y.f][y.s] = dist[x.f][x.s]+1;
-            todo.push(y);
+            todo.push(y); // push point to queue
         }
     }
 }
@@ -43,10 +43,10 @@ void process(pii x) {
 int main() {
 	ios_base::sync_with_stdio(0);cin.tie(0);
 	F0R(i,21) F0R(j,21) dist[i][j] = MOD;
-	dist[10][10] = 0; todo.push({10,10});
+	dist[10][10] = 0; todo.push({10,10}); // initialize queue, distances
 	while (todo.size()) {
 	    process(todo.front());
-	    todo.pop();
+	    todo.pop(); // pop point from queue
 	}
 	cout << dist[4][5]; // 11
 }

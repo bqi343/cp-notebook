ef0e0bc5cfb9de4244443b0c77ee72e2c86879a4
Benjamin Qi <bqi343@gmail.com>
2017-12-01 19:57:16 -0500

    allow duplicates

--- List of Topics/(2+) Data Structures/Implementations/2D Range Queries/(Plat) Merge-Sort Tree*.cpp
+++ List of Topics/(2+) Data Structures/Implementations/2D Range Queries/(Plat) Merge-Sort Tree*.cpp
@@ -1,13 +1,16 @@
 template<int SZ> struct mstree { 
-    Tree<int> val[SZ+1]; // for offline queries use vector with binary search instead
+    Tree<pii> val[SZ+1]; // for offline queries use vector with binary search instead
     
-    void upd(int x, int y) { // x-coordinate between 1 and SZ inclusive
-        for (;x <= SZ; x += (x&-x)) val[x].insert(y);
+    void upd(int x, int y, int t = 1) { // x-coordinate between 1 and SZ inclusive
+        for (;x <= SZ; x += (x&-x)) {
+            if (t) val[x].insert({y,x});
+            else val[x].erase({y,x});
+        }
     }
     
     int query(int x, int y) {
         int t = 0;    
-        for (;x > 0; x -= (x&-x)) t += val[x].order_of_key(y+1);
+        for (;x > 0; x -= (x&-x)) t += val[x].order_of_key({y,MOD});
         return t;
     }
     
@@ -19,8 +22,5 @@ template<int SZ> struct mstree {
 int main() {
 	mstree<100000> m;
 	m.upd(3,6); m.upd(4,5);
-	cout << m.query(3,5,4,6) << " " << m.query(3,5,4,5);
+	cout << m.query(3,5,4,6) << " " << m.query(3,5,4,5); // 2, 1
 }
-
-// read!
-// ll vs. int!

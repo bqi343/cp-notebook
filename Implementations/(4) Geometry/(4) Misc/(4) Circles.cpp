a443a7dbf03ac28de26296f51fbcd5e7b3761b81
Benjamin Qi <bqi343@gmail.com>
2017-12-20 14:02:16 -0500

    fix circles

--- Implementations/(4) Geometry/(4) Misc/(4) Circles.cpp
+++ Implementations/(4) Geometry/(4) Misc/(4) Circles.cpp
@@ -7,7 +7,7 @@ typedef pair<cd,double> circle;
 
 cd intersect(circle a, circle b, int x = 0) { 
     double d = sqrt(norm(a.f-b.f));
-    double co = (a.s*a.s+b.s*b.s-d*d)/(2*a.s*b.s);
+    double co = (a.s*a.s+d*d-b.s*b.s)/(2*a.s*d);
     double theta = acos(co);
     
     cd tmp = (b.f-a.f)/d;
@@ -16,11 +16,16 @@ cd intersect(circle a, circle b, int x = 0) {
 }
 
 double arc(circle x, cd a, cd b) {
-	cd d = (a-x.f)/(b-x.f);
-	return x.s*acos(d.real());
+    cd d = (a-x.f)/(b-x.f);
+    return x.s*acos(d.real());
+}
+
+bool on (circle x, cd y) {
+    return norm(y-x.f) == x.s*x.s;
 }
 
 int main() {
-	cout << intersect({0,2},{1,1}) << "\n";
-	cout << arc({0,1},cd(1,0),cd(0,1));
+    cout << intersect({0,2},{1,1}) << "\n";
+    cout << arc({0,1},cd(1,0),cd(0,1)) << "\n";
+    cout << on({0,1},1) << "\n";
 }

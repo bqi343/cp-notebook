4cdc5b6a499b32652c28a20e71fe8f36ccfa4411
Benjamin Qi <bqi343@gmail.com>
2018-01-05 17:48:28 -0500

    Create (4) LCA Demo.cpp

---
+++ Implementations/(3) Trees/(4) Tree Queries/(4) LCA Demo.cpp
@@ -0,0 +1,13 @@
+int main() {
+    cin >> L.V >> Q >> L.R;
+    F0R(i,L.V-1) {
+        int u,v; cin >> u >> v;
+        L.addEdge(u,v);
+    }
+    L.construct();
+    
+    F0R(i,Q) {
+        int u,v; cin >> u >> v;
+        cout << L.lca(u,v) << "\n";
+    }
+}

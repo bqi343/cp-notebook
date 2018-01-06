f79e9a2a958eb192bea7afe684020c9868540659
Benjamin Qi <bqi343@gmail.com>
2018-01-05 19:13:58 -0500

    Update (4) Merge-Sort Tree.cpp

--- Implementations/(2) Data Structures/(4) 2D Range Queries/(4) Merge-Sort Tree.cpp
+++ Implementations/(2) Data Structures/(4) 2D Range Queries/(4) Merge-Sort Tree.cpp
@@ -1,6 +1,7 @@
 /**
 * Description: Similar to 2D segtree, less memory
 * For more complex queries use a customized treap
+* Verification: http://codeforces.com/contest/785/submission/33953058
 */
 
 template<int SZ> struct mstree { 
@@ -8,7 +9,7 @@ template<int SZ> struct mstree {
     
     void upd(int x, int y, int t = 1) { // x-coordinate between 1 and SZ inclusive
         for (int X = x; X <= SZ; X += X&-X) {
-            if (t) val[X].insert({y,x});
+            if (t == 1) val[X].insert({y,x});
             else val[X].erase({y,x});
         }
     }

135339923eeba1790ac2e93a45947d214210d7ab
Benjamin Qi <bqi343@gmail.com>
2018-01-07 12:11:40 -0500

    Update (2) Prefix Sums.cpp

--- Implementations/(2) Data Structures/(2) 2D Range Queries/(2) Prefix Sums.cpp
+++ Implementations/(2) Data Structures/(2) 2D Range Queries/(2) Prefix Sums.cpp
@@ -1,15 +1,16 @@
 /**
  * Description: Calculates rectangle sums in constant time
+ * Verification: POI 16 Ticket Inspector
  */
 
 template<class T, int SZ> struct sums {
-    ll sum[SZ][SZ];
+    T sum[SZ][SZ];
     sums () { memset(sum,0,sizeof sum); }
     void init() {
         FOR(i,1,SZ) FOR(j,1,SZ) 
             sum[i][j] += sum[i][j-1]+sum[i-1][j]-sum[i-1][j-1];
     }
-    ll get(int X1, int X2, int Y1, int Y2) {
+    T get(int X1, int X2, int Y1, int Y2) {
         return sum[X2][Y2]-sum[X1-1][Y2]
         		-sum[X2][Y1-1]+sum[X1-1][Y1-1];
     }

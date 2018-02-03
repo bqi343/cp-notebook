eb3a017b1ef00e770b6a76af94d83d242d7517d7
Benjamin Qi <bqi343@gmail.com>
2018-02-03 08:49:32 -0500

    FFT

---
+++ Implementations/(4) Math/(6) Polynomials/(6) And Convolution.cpp
@@ -0,0 +1,76 @@
+/**
+* Description: Similar to FWHT
+* Source: https://csacademy.com/blog/fast-fourier-transform-and-variations-of-it
+*/
+
+typedef vector<double> vd;
+typedef vector<ll> vl;
+
+int get(int s) {
+    return s > 1 ? 32 - __builtin_clz(s - 1) : 0;
+}
+
+namespace andConv {
+    vd andConv(vd P, bool inv = 0) {
+        for (int len = 1; 2 * len <= sz(P); len <<= 1) {
+            for (int i = 0; i < sz(P); i += 2 * len) {
+                for (int j = 0; j < len; j++) {
+                    double u = P[i + j];
+                    double v = P[i + len + j];
+
+                    if (!inv) {
+                        P[i + j] = v;
+                        P[i + len + j] = u + v;
+                    } else {
+                        P[i + j] = -u + v;
+                        P[i + len + j] = u;
+                    }
+                }
+            }
+        }
+    
+        return P;
+    }
+    
+    vd conv(vd a, vd b) {
+        int s = max(sz(a),sz(b)), L = get(s), n = 1<<L;
+        if (s <= 0) return {};
+        
+        a.resize(n); a = andConv(a);
+        b.resize(n); b = andConv(b);
+        
+        F0R(i,n) a[i] = a[i]*b[i];
+        a = andConv(a,1);
+        return a;
+    }
+    
+    vd orConv(vd a, vd b) {
+        int s = max(sz(a),sz(b)), L = get(s), n = 1<<L;
+        if (s <= 0) return {};
+        
+        a.resize(n); reverse(all(a)); a = andConv(a);
+        b.resize(n); reverse(all(b)); b = andConv(b);
+        
+        F0R(i,n) a[i] = a[i]*b[i];
+        a = andConv(a,1);
+        reverse(all(a));
+        
+        return a;
+    }
+    
+    vl orConv(vl a, vl b) {
+        vd A; for (ll x: a) A.pb(x);
+        vd B; for (ll x: b) B.pb(x);
+        vd c = orConv(A,B);
+        vl C; for (double x: c) C.pb(round(x));
+        return C;
+    }
+    
+    vl conv(vl a, vl b) {
+        vd A; for (ll x: a) A.pb(x);
+        vd B; for (ll x: b) B.pb(x);
+        vd c = conv(A,B);
+        vl C; for (double x: c) C.pb(round(x));
+        return C;
+    }
+}
---
+++ Implementations/(4) Math/(6) Polynomials/(6) XOR Convolution.cpp
@@ -0,0 +1,56 @@
+/**
+* Description: FWHT, similar to FFT
+* Source: https://csacademy.com/blog/fast-fourier-transform-and-variations-of-it
+* Verification: https://www.hackerrank.com/challenges/xor-subsequence/problem
+*/
+
+typedef vector<double> vd;
+typedef vector<ll> vl;
+
+int get(int s) {
+    return s > 1 ? 32 - __builtin_clz(s - 1) : 0;
+}
+
+namespace FWHT {
+    vd fwht(vd P) {
+        for (int len = 1; 2 * len <= sz(P); len <<= 1) {
+            for (int i = 0; i < sz(P); i += 2 * len) {
+                for (int j = 0; j < len; j++) {
+                    double u = P[i + j];
+                    double v = P[i + len + j];
+                    P[i + j] = u+v;
+                    P[i + len + j] = u-v;
+                }
+            }
+        }
+    
+        return P;
+    }
+
+    
+    vd fwht_rev(vd& a) {
+        vd res = fwht(a);
+        F0R(i,sz(res)) res[i] /= a.size();
+        return res;
+    }
+    
+    vd conv(vd a, vd b) {
+        int s = max(sz(a),sz(b)), L = get(s), n = 1<<L;
+        if (s <= 0) return {};
+        
+        a.resize(n); a = fwht(a);
+        b.resize(n); b = fwht(b);
+        
+        F0R(i,n) a[i] = a[i]*b[i];
+        a = fwht_rev(a);
+        return a;
+    }
+    
+    vl conv(vl a, vl b) {
+        vd A; for (ll x: a) A.pb(x);
+        vd B; for (ll x: b) B.pb(x);
+        vd c = conv(A,B);
+        vl C; for (double x: c) C.pb(round(x));
+        return C;
+    }
+}

42c78199205d6181fb1b18ea854aec90a2a5c8a9
Benjamin Qi <bqi343@gmail.com>
2018-02-05 18:17:33 -0500

    SegTree

--- Implementations/(2) Data Structures/(2) 1D Range Queries/(3) SegTree.cpp
+++ Implementations/(2) Data Structures/(2) 1D Range Queries/(3) SegTree.cpp
@@ -14,15 +14,8 @@ template<class T, int SZ> struct Seg {
     T comb(T a, T b) { return a+b; } // easily change this to min or max
     
     void upd(int p, T value) {  // set value at position p
-        for (seg[p += SZ] = value; p > 1; p >>= 1){ 
-            //be careful here: some updates are noncommutative! 
-            if (p < (p^1)){
-                 seg[p>>1] = comb(seg[p],seg[p^1]);
-            }
-            else{
-                 seg[p>>1] = comb(seg[p^1], seg[p]);    
-            }
-        }
+        for (seg[p += SZ] = value; p > 1; p >>= 1)
+            seg[p>>1] = comb(seg[(p|1)^1],seg[p|1]); // non-commutative operations
     }
     
     void build() {
@@ -30,11 +23,11 @@ template<class T, int SZ> struct Seg {
     }
     
     T query(int l, int r) {  // sum on interval [l, r]
-        T res = MN; r++;
+        T res1 = MN, res2 = MN; r++;
         for (l += SZ, r += SZ; l < r; l >>= 1, r >>= 1) {
-            if (l&1) res = comb(res,seg[l++]);
-            if (r&1) res = comb(res,seg[--r]);
+            if (l&1) res1 = comb(res1,seg[l++]);
+            if (r&1) res2 = comb(seg[--r],res2);
         }
-        return res;
+        return comb(res1,res2);
     }
 };

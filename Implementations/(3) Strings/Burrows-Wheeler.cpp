a1903bacbe4ace97f3b01e164014c023ba13ddf1
Benjamin Qi <bqi343@gmail.com>
2018-02-06 12:10:41 -0500

    Create Burrows-Wheeler.cpp

---
+++ Implementations/(3) Strings/Burrows-Wheeler.cpp
@@ -0,0 +1,21 @@
+/**
+* Description: Reverses Burrows-Wheeler, if valid
+* Verification: https://cses.fi/problemset/task/1113/
+*/
+
+string transform(string s) {
+	vector<pair<char,int>> v;
+	int nex[sz(s)];
+
+	F0R(i,sz(s)) v.pb({s[i],i});
+	sort(all(v));
+	F0R(i,sz(v)) nex[i] = v[i].s;
+
+	int cur = nex[0];
+	string ret;
+	while (cur != 0) {
+		ret += v[cur].f;
+		cur = nex[cur];
+	}
+	return ret;
+}

7d04c67a87a8efde2859f22cfff7065164432574
Benjamin Qi <bqi343@gmail.com>
2018-02-17 22:12:12 -0500

    Update and rename Distinct Subsequences.cpp to String Removals.cpp

--- Implementations/06 - DP (3)/6.2 - Examples/String Removals.cpp
+++ Implementations/06 - DP (3)/6.2 - Examples/String Removals.cpp
@@ -1,13 +1,14 @@
 /**
 * Description: DP eliminates overcounting
+* Verification: https://cses.fi/problemset/task/1149/
 */
 
 int distinct(string S) {
     vi tot(26);
     int ans = 1;
     for (char c: S) {
-        int t = (ans-tot[c-'A']+MOD)%MOD;
-        tot[c-'A'] = (tot[c-'A']+t)%MOD;
+        int t = (ans-tot[c-'a']+MOD)%MOD;
+        tot[c-'a'] = (tot[c-'a']+t)%MOD;
         ans = (ans+t)%MOD;
     }
     return ans;

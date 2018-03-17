f032ab9fe9071bc34c5b379a55362d1a009b0d67
Benjamin Qi <bqi343@gmail.com>
2018-03-17 10:51:48 -0400

    Update 10.1 - Tree Diameter.cpp

--- Implementations/10 - Trees (4)/10.1 - Tree Diameter.cpp
+++ Implementations/10 - Trees (4)/10.1 - Tree Diameter.cpp
@@ -3,28 +3,37 @@
 * Verification: http://www.spoj.com/problems/PT07Z/
 */
 
-const int MX = 10001;
-
-int n, dist[MX];
+int n, dist[MX], pre[MX];
 vi adj[MX];
 
-void dfs(int cur, int pre) {
-    for (int i: adj[cur]) if (i != pre) {
+void dfs(int cur) {
+    for (int i: adj[cur]) if (i != pre[cur]) {
+        pre[i] = cur;
         dist[i] = dist[cur]+1;
-        dfs(i,cur);
+        dfs(i);
     }
 }
 
-void dfs(int cur) {
+void genDist(int cur) {
     memset(dist,0,sizeof dist);
-    dfs(cur,-1);
+    pre[cur] = -1;
+    dfs(cur);
 }
 
 int treeDiameter() {
-    dfs(1);
+    genDist(1);
     int bes = 0; FOR(i,1,n+1) if (dist[i] > dist[bes]) bes = i;
-    dfs(bes); FOR(i,1,n+1) if (dist[i] > dist[bes]) bes = i;
+    genDist(bes); FOR(i,1,n+1) if (dist[i] > dist[bes]) bes = i;
     return dist[bes];
+}
+
+vi genCenter() {
+    int t = treeDiameter();
+    int bes = 0; FOR(i,1,n+1) if (dist[i] > dist[bes]) bes = i;
+    
+    F0R(i,t/2) bes = pre[bes];
+    if (t&1) return {bes,pre[bes]};
+    return {bes};
 }
 
 int main() {
@@ -33,5 +42,6 @@ int main() {
         int a, b; cin >> a >> b;
         adj[a].pb(b), adj[b].pb(a);
     }
-    cout << treeDiameter();
+    vi x = genCenter();
+    for (int i: x) cout << i << " ";
 }

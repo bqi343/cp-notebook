29d4229c5ae015ff4759d32e16b879b0d1318a1c
Benjamin Qi <bqi343@gmail.com>
2018-04-12 13:12:51 -0400

    Update Push-Relabel (5).cpp

--- Implementations/12 - Graphs Hard (4)/12.3 - Flows/Push-Relabel (5).cpp
+++ Implementations/12 - Graphs Hard (4)/12.3 - Flows/Push-Relabel (5).cpp
@@ -16,7 +16,7 @@ template <int SZ> struct PushRelabel {
     bool active[SZ];
     vi B[SZ];
     
-    void addEdge(int u, int v, int C) {
+    void addEdge(int u, int v, ll C) {
         Edge a{v, 0, C, sz(adj[v])};
         Edge b{u, 0, 0, sz(adj[u])};
         adj[u].pb(a), adj[v].pb(b); 

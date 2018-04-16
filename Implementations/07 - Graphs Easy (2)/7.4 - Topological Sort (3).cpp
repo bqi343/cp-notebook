1ff5c93b4b3b293407a83a1ce9d68b5c34ae23d3
Benjamin Qi <bqi343@gmail.com>
2018-04-16 11:24:30 -0400

    Update 7.4 - Topological Sort (3).cpp

--- Implementations/07 - Graphs Easy (2)/7.4 - Topological Sort (3).cpp
+++ Implementations/07 - Graphs Easy (2)/7.4 - Topological Sort (3).cpp
@@ -2,28 +2,24 @@
 * Description: sorts vertices such that if there exists an edge x->y, then x goes before y
 */
 
-int N,M, in[100001];
-vi res, adj[100001];
-
-void topo() {
-    queue<int> todo;
-    FOR(i,1,N+1) if (in[i] == 0) todo.push(i);
-    while (sz(todo)) {
-        int x = todo.front(); todo.pop();
-        res.pb(x);
-        for (int i: adj[x]) {
-            in[i] --;
-            if (!in[i]) todo.push(i);
+struct Topo {
+    int N, in[MX];
+    vi res, adj[MX];
+    
+    void addEdge(int x, int y) {
+    	adj[x].pb(y), in[y] ++;
+    }
+    
+    void sort() {
+        queue<int> todo;
+        FOR(i,1,N+1) if (in[i] == 0) todo.push(i);
+        while (sz(todo)) {
+            int x = todo.front(); todo.pop();
+            res.pb(x);
+            for (int i: adj[x]) {
+                in[i] --;
+                if (!in[i]) todo.push(i);
+            }
         }
     }
-}
-
-int main() {
-	cin >> N >> M;
-	F0R(i,M) {
-	    int x,y; cin >> x >> y;
-	    adj[x].pb(y), in[y] ++;
-	}
-	topo();
-	for (int i: res) cout << i << " ";
-}
+};

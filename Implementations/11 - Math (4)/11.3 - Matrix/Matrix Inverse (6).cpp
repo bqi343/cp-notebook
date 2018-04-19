cef95797faeb8c9db43e05553c16d1f416486c7a
Benjamin Qi <bqi343@gmail.com>
2018-04-18 20:47:37 -0400

    Update Matrix Inverse (6).cpp

--- Implementations/11 - Math (4)/11.3 - Matrix/Matrix Inverse (6).cpp
+++ Implementations/11 - Math (4)/11.3 - Matrix/Matrix Inverse (6).cpp
@@ -6,12 +6,13 @@
 ll po (ll b, ll p) { return !p?1:po(b*b%MOD,p/2)*(p&1?b:1)%MOD; }
 ll inv (ll b) { return po(b,MOD-2); }
 
-int mul(ll a, ll b) { return a*b%MOD; }
-int sub(ll a, ll b) { return ((a-b)%MOD+MOD)%MOD; }
+int ad(int a, int b) { return (a+b)%MOD; }
+int sub(int a, int b) { return (a-b+MOD)%MOD; }
+int mul(int a, int b) { return (ll)a*b%MOD; }
 
 void elim(mat& m, int a, int c) { // column, todo row
     ll x = m.d[c][a];
-    FOR(i,a,m.b) m.d[c][i] = sub(m.d[c][i],x*m.d[a][i]);
+    FOR(i,a,m.b) m.d[c][i] = sub(m.d[c][i],mul(x,m.d[a][i]));
 }
 
 int det(mat& x, bool b = 0) { // determinant of 1000x1000 matrix in ~1s

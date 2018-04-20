55fe8134871be12cad67e2ed756033985a3ccca1
Benjamin Qi <bqi343@gmail.com>
2018-04-20 19:04:29 -0400

    Circles

--- Implementations/13 - Geometry (4)/13.1 - Techniques/Circles (6).cpp
+++ Implementations/13 - Geometry (4)/13.1 - Techniques/Circles (6).cpp
@@ -3,29 +3,23 @@
 * Usage: https://codefights.com/tournaments/s8thqrnQL2YPK7XQt/L
 */
 
-typedef pair<cd,double> circle;
+typedef pair<cd,ld> circle;
 
 cd intersect(circle a, circle b, int x = 0) { 
-    double d = sqrt(norm(a.f-b.f));
-    double co = (a.s*a.s+d*d-b.s*b.s)/(2*a.s*d);
-    double theta = acos(co);
+    ld d = sqrt(norm(a.f-b.f));
+    ld co = (a.s*a.s+d*d-b.s*b.s)/(2*a.s*d);
+    ld theta = acos(co);
     
     cd tmp = (b.f-a.f)/d;
-    if (x == 0) return a.f+tmp*a.s*polar(1.0,theta);
-    return a.f+tmp*a.s*polar(1.0,-theta);
+    if (x == 0) return a.f+tmp*a.s*polar((ld)1.0,theta);
+    return a.f+tmp*a.s*polar((ld)1.0,-theta);
 }
 
-double arc(circle x, cd a, cd b) {
+ld arc(circle x, cd a, cd b) {
     cd d = (a-x.f)/(b-x.f);
     return x.s*acos(d.real());
 }
 
 bool on (circle x, cd y) {
     return norm(y-x.f) == x.s*x.s;
-}
-
-int main() {
-    cout << intersect({0,2},{1,1}) << "\n";
-    cout << arc({0,1},cd(1,0),cd(0,1)) << "\n";
-    cout << on({0,1},1) << "\n";
 }

11d2fa9fcaeafa470124c308230424450e71f02a
Benjamin Qi <bqi343@gmail.com>
2018-04-25 10:44:27 -0400

    LSI

--- Implementations/13 - Geometry (4)/13.1 - Techniques/Line Segment Intersection (5).cpp
+++ Implementations/13 - Geometry (4)/13.1 - Techniques/Line Segment Intersection (5).cpp
@@ -3,28 +3,48 @@
 * If numbers are small enough, fractions are recommended.
 */
 
-cd get(cd a, cd b, cd c, cd d) {
-    return (abs(sgn(a,b,c))*d+abs(sgn(a,b,d))*c)
-     /(abs(sgn(a,b,c))+abs(sgn(a,b,d)));
-}
-
-vcd solve(cd A, cd B, cd C, cd D) {
-    if (A > B) swap(A,B);
-    if (C > D) swap(C,D);
-
-    ld a1 = sgn(A,B,C), a2 = sgn(A,B,D);
-    if (a1 > a2) swap(a1,a2);
-    if (!(a1 <= 0 && a2 >= 0)) return {};
-
-    if (a1 == 0 && a2 == 0) {
-        if (sgn(A,C,D) != 0) return {};
-        cd x1 = max(A,C), x2 = min(B,D);
-        if (x1 > x2) return {};
-        if (x1 == x2) return {x1};
-        return {x1,x2};
+namespace intersect {
+    bool operator<(const cd& a, const cd& b) {
+        if (a.real() != b.real()) return a.real() < b.real();
+        return a.imag() < b.imag();
     }
     
-    cd z = get(A,B,C,D);
-    if (A <= z && z <= B) return {z};
-    return {};
-}
+    bool operator>(const cd& a, const cd& b) {
+        if (a.real() != b.real()) return a.real() > b.real();
+        return a.imag() > b.imag();
+    }
+    
+    bool operator<=(const cd& a, const cd& b) { return a < b || a == b; }
+    bool operator>=(const cd& a, const cd& b) { return a > b || a == b; }
+    cd max(const cd& a, const cd& b) { return a>b?a:b; }
+    cd min(const cd& a, const cd& b) { return a<b?a:b; }
+
+    ld cross(cd b, cd c) { return (conj(b)*c).imag(); }
+    ld cross(cd a, cd b, cd c) { return cross(b-a,c-a); }
+
+    cd line(cd a, cd b, cd c, cd d) {
+        return (cross(a,b,c)*d-cross(a,b,d)*c)
+         /(cross(a,b,c)-cross(a,b,d));
+    }
+
+    vcd segment(cd A, cd B, cd C, cd D) {
+        if (A > B) swap(A,B);
+        if (C > D) swap(C,D);
+
+        ld a1 = cross(A,B,C), a2 = cross(A,B,D);
+        if (a1 > a2) swap(a1,a2);
+        if (!(a1 <= 0 && a2 >= 0)) return {};
+
+        if (a1 == 0 && a2 == 0) {
+            if (cross(A,C,D) != 0) return {};
+            cd x1 = max(A,C), x2 = min(B,D);
+            if (x1 > x2) return {};
+            if (x1 == x2) return {x1};
+            return {x1,x2};
+        }
+        
+        cd z = line(A,B,C,D);
+        if (A <= z && z <= B) return {z};
+        return {};
+    }
+};

be3451b261f0d9839e6c323b792958c463d9b51c
Benjamin Qi <bqi343@gmail.com>
2018-04-29 16:05:28 -0400

    Update 14.1 - Bitset Trie (4).cpp

--- Implementations/14 - Strings (3)/14.1 - Bitset Trie (4).cpp
+++ Implementations/14 - Strings (3)/14.1 - Bitset Trie (4).cpp
@@ -4,29 +4,32 @@
 */
 
 template<int MX> struct tri {
-    int nex = 0, ans = 0;
-    int trie[MX][2]; // easily changed to character
+    const int MXBIT = 60;
+    int trie[MX][2], nex = 0; // easily changed to character
+    int sz[MX][2];
+    ll ans = 0;
     
     tri() {
         memset(trie,0,sizeof trie);
     }
     
-    void ins(int x) {
-        int cur = 0;
-        F0Rd(i,30) {
-            int t = (x&(1<<i))>>i;
+    void ins(ll x, int a = 1) { // insert or delete
+        int cur = 0; sz[cur] += a;
+        F0Rd(i,MXBIT) {
+            int t = (x&(1LL<<i))>>i;
             if (!trie[cur][t]) trie[cur][t] = ++nex;
             cur = trie[cur][t];
+            sz[cur] += a;
         }
     }
     
-    void test(int x) {
+    void test(ll x) {
         int cur = 0;
-        F0Rd(i,30) {
-            int t = ((x&(1<<i))>>i) ^ 1;
-            if (!trie[cur][t]) t ^= 1;
+        F0Rd(i,MXBIT) {
+            int t = ((x&(1LL<<i))>>i) ^ 1;
+            if (!trie[cur][t] || !sz[trie[cur][t]]) t ^= 1;
             cur = trie[cur][t];
-            if (t) x ^= (1<<i);
+            if (t) x ^= (1LL<<i);
         }
         ans = max(ans,x);
     }

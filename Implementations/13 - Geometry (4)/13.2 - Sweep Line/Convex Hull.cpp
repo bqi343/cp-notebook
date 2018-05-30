3d8b3d7e37f3599e79ffe8e11612ade7149371ea
Benjamin Qi <bqi343@gmail.com>
2018-05-29 23:41:51 -0400

    Convex Hull

--- Implementations/13 - Geometry (4)/13.2 - Sweep Line/Convex Hull.cpp
+++ Implementations/13 - Geometry (4)/13.2 - Sweep Line/Convex Hull.cpp
@@ -10,20 +10,19 @@ ll cross(pi O, pi A, pi B) {
 
 vpi convex_hull(vpi P) {
     sort(all(P)); P.erase(unique(all(P)),P.end());
-    if (P.size() == 1) return P;
-    
-    int n = P.size();
+    int n = sz(P);
+    if (n == 1) return P;
     
     vpi bot = {P[0]};
     FOR(i,1,n) {
-        while (bot.size() > 1 && cross(bot[sz(bot)-2], bot.back(), P[i]) <= 0) bot.pop_back();
+        while (sz(bot) > 1 && cross(bot[sz(bot)-2], bot.back(), P[i]) <= 0) bot.pop_back();
         bot.pb(P[i]);
     }
     bot.pop_back();
     
     vpi up = {P[n-1]};
     F0Rd(i,n-1) {
-        while (up.size() > 1 && cross(up[sz(up)-2], up.back(), P[i]) <= 0) up.pop_back();
+        while (sz(up) > 1 && cross(up[sz(up)-2], up.back(), P[i]) <= 0) up.pop_back();
         up.pb(P[i]);
     }
     up.pop_back();

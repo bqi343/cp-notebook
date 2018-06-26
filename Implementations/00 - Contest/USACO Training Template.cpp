88be97dbf354029a582df31405af7c1494f0e009
Benjamin Qi <bqi343@gmail.com>
2018-06-26 17:36:56 -0400

    Create USACO Training Template.cpp

---
+++ Implementations/00 - Contest/USACO Training Template.cpp
@@ -0,0 +1,53 @@
+/*
+ID: bqi3431
+PROG: 
+LANG: C++
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+
+const int MOD = 1000000007;
+const ll INF = 1e18;
+const int MX = 100001;
+
+int main() {
+    freopen(".in","r",stdin);
+    freopen(".out","w",stdout);
+    
+    // don't forget newline!
+}
+
+
+// read the question correctly (is y a vowel? what are the exact constraints?)
+// look out for SPECIAL CASES (n=1?) and overflow (ll vs int?) ARRAY OUT OF BOUNDS

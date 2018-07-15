46d93a5e6d4a82f6d4d411e612d7f38f92b34751
Benjamin Qi <bqi343@gmail.com>
2018-07-15 15:57:05 -0400

    Update Centroid Decomposition.cpp

--- Implementations/10 - Trees (4)/10.3 - Advanced/Centroid Decomposition.cpp
+++ Implementations/10 - Trees (4)/10.3 - Advanced/Centroid Decomposition.cpp
@@ -1,47 +1,71 @@
 /**
 * Source: own
-* Verification Problem: Ciel and Commander (http://codeforces.com/contest/321/problem/C)
-* Code: http://codeforces.com/contest/321/submission/33952270
+* Verification: https://codeforces.com/contest/342/problem/E
+* Description: supports the following operations on a tree
+ * making node red
+ * querying distance to closest red node
 */
 
-const int MX = 100001;
-
-int N, visit[MX], sub[MX], par[MX];
-vi adj[MX];
-
-void dfs (int no) {
-    sub[no] = 1;
-    for (int i: adj[no]) if (!visit[i] && i != par[no]) {
-        par[i] = no;
-        dfs(i);
-        sub[no] += sub[i];
+template<int SZ> struct centroidDecomp {
+    int N;
+    bool done[SZ];
+    int sub[SZ], par[SZ], ans[SZ], cen[SZ];
+    vi dist[SZ], adj[SZ];
+    
+    // INITIALIZE
+    
+    void addEdge(int a, int b) { adj[a].pb(b), adj[b].pb(a); }
+    
+    void dfs (int no) {
+        sub[no] = 1;
+        for (int i: adj[no]) if (!done[i] && i != par[no]) {
+            par[i] = no;
+            dfs(i);
+            sub[no] += sub[i];
+        }
     }
-}
-
-int get_centroid(int x) {
-    par[x] = 0;
-    dfs(x);
-    int sz = sub[x];
-    while (1) {
-        pi mx = {0,0};
-        for (int i: adj[x]) if (!visit[i] && i != par[x]) mx = max(mx,{sub[i],i});
-        if (mx.f*2 > sz) x = mx.s;
-        else return x;
+    
+    void genDist(int par, int no, int t, int dis) {
+        dist[no].pb(dis);
+        for (int i: adj[no]) if (!done[i] && i != par) {
+            cen[i] = t;
+            genDist(no,i,t,dis+1);
+        }
     }
-}
-
-void solve (int x) {
-    x = get_centroid(x); visit[x] = 1;
-    // do stuff
-    cout << x << "\n";
-    for (int i: adj[x]) if (!visit[i]) solve(i);
-}
-
-int main() {
-	cin >> N;
-	F0R(i,N-1) {
-	    int a,b; cin >> a >> b;
-	    adj[a].pb(b), adj[b].pb(a);
-	}
-	solve(1);
-}
+    
+    int getCentroid(int x) {
+        par[x] = 0; dfs(x);
+        int sz = sub[x];
+        while (1) {
+            pi mx = {0,0};
+            for (int i: adj[x]) if (!done[i] && i != par[x]) mx = max(mx,{sub[i],i});
+            if (mx.f*2 > sz) x = mx.s;
+            else return x;
+        }
+    }
+    
+    void solve (int x) {
+        x = getCentroid(x); done[x] = 1;
+        genDist(0,x,x,0);
+        for (int i: adj[x]) if (!done[i]) solve(i);
+    }
+    
+    void init() {
+    	FOR(i,1,N+1) ans[i] = MOD;
+    	solve(1);
+    }
+    
+    // QUERY 
+    
+    void upd(int v) {
+        for (int V = v, ind = sz(dist[v])-1; V; V = cen[V], ind --) 
+            ans[V] = min(ans[V],dist[v][ind]);
+    }
+    
+    int query(int v) {
+        int ret = MOD;
+        for (int V = v, ind = sz(dist[v])-1; V; V = cen[V], ind --) 
+            ret = min(ret,ans[V]+dist[v][ind]);
+        return ret;
+    }
+};

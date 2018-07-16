f188d0e72f831e4f66b3ddbfd18b72e404c21634
Benjamin Qi <bqi343@gmail.com>
2018-07-15 21:42:54 -0400

    Update 15.1 - Mo.cpp

--- Implementations/15 - Additional (4)/15.1 - Mo.cpp
+++ Implementations/15 - Additional (4)/15.1 - Mo.cpp
@@ -4,9 +4,27 @@
 * Also see Mo's on trees
 */
 
-int block = 300; // set ~sqrt(N)
+int N, Q, A[MX], ans[MX], oc[MX];
+vector<array<int,3>> todo;
 
-bool cmp(vi a, vi b) {
-    if (a[0]/block != b[0]/block) return a[0] < b[0];
+bool cmp(array<int,3> a, array<int,3> b) { // sort queries
+    if (a[0]/sqrt(N) != b[0]/sqrt(N)) return a[0] < b[0];
     return a[1] < b[1];
+}
+
+int l = 0, r = -1, cans = 0;
+
+void ad(int x, int y = 1) {
+    x = A[x];
+    // if condition: cans --;
+    oc[x] += y;
+    // if condition: cans ++;
+}
+
+int answer(int L, int R) { // adjust interval
+    while (l > L) ad(--l);
+    while (r < R) ad(++r);
+    while (l < L) ad(l++,-1);
+    while (r > R) ad(r--,-1);
+    return cans;
 }

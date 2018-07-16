100213ccf2036cc89facee0ebc582048955694f5
Benq <bqi343@gmail.com>
2018-07-16 12:06:37 -0400

    Max Rectangle

---
+++ Implementations/13 - Geometry (4)/13.2 - Sweep Line/Max Rectangle.cpp
@@ -0,0 +1,30 @@
+/**
+* Description: Computes size of max rectangle in grid w/ obstacles
+* Verification: https://cses.fi/problemset/task/1147/
+*/
+
+int n,m,cur[1000];
+char g[1000][1000];
+ll ans = 0;
+ 
+void solve(int x) {
+    vi nex[m+1];
+    F0R(i,n) nex[cur[i]-x].pb(i);
+    
+    DSU<1000> D = DSU<1000>();
+    F0Rd(i,m+1) for (int a: nex[i]) {
+        D.par[a] = a;
+        if (a > 0 && D.par[a-1] != -1) D.unite(a,a-1);
+        if (a < n-1 && D.par[a+1] != -1) D.unite(a,a+1);
+        ans = max(ans,i*(ll)D.sz[D.get(a)]);
+    }
+}
+ 
+int solve() {
+    F0R(i,n) cur[i] = m;
+    F0Rd(j,m) {
+        F0R(i,n) if (g[i][j] == '*') cur[i] = j; // obstacle
+        solve(j);
+    }
+    return ans;
+}

1c99a89476f578c78d50225c50d9a2c44e0d1748
Benq <bqi343@gmail.com>
2018-07-18 15:49:03 -0400

    Fraction + Mod

--- Implementations/11 - Math (4)/11.1 - Number Theory/Fraction.cpp
+++ Implementations/11 - Math (4)/11.1 - Number Theory/Fraction.cpp
@@ -5,7 +5,6 @@
 
 struct Fraction {
     ll n,d;
-    
     Fraction() { n = 0, d = 1; }
     Fraction(ll _n, ll _d) {
         n = _n, d = _d;
@@ -17,59 +16,26 @@ struct Fraction {
 
 Fraction abs(Fraction F) { return Fraction(abs(F.n),F.d); }
     
-bool operator<(const Fraction& lhs, const Fraction& rhs) {
-    return lhs.n*rhs.d < rhs.n*lhs.d;
-}
+bool operator<(const Fraction& l, const Fraction& r) { return l.n*r.d < r.n*l.d; }
+bool operator==(const Fraction& l, const Fraction& r) { return l.n == r.n && l.d == r.d; }
+bool operator!=(const Fraction& l, const Fraction& r) { return !(l == r); }
 
-bool operator==(const Fraction& lhs, const Fraction& rhs) {
-    return lhs.n == rhs.n && lhs.d == rhs.d;
-}
+Fraction operator+(const Fraction& l, const Fraction& r) { return Fraction(l.n*r.d+r.n*l.d,l.d*r.d); }
+Fraction operator-(const Fraction& l, const Fraction& r) { return Fraction(l.n*r.d-r.n*l.d,l.d*r.d); }
+Fraction operator*(const Fraction& l, const Fraction& r) { return Fraction(l.n*r.n,l.d*r.d); }
+Fraction operator*(const Fraction& l, int r) { return l*Fraction(r,1); }
+Fraction operator*(int r, const Fraction& l) { return l*r; }
+Fraction operator/(const Fraction& l, const Fraction& r) { return l*Fraction(r.d,r.n); }
+Fraction operator/(const Fraction& l, const int& r) { return l/Fraction(r,1); }
+Fraction operator/(const int& l, const Fraction& r) { return Fraction(l,1)/r; }
 
-bool operator!=(const Fraction& lhs, const Fraction& rhs) { 
-    return !(lhs == rhs); 
-}
-
-Fraction operator+(const Fraction& lhs, const Fraction& rhs) {
-    return Fraction(lhs.n*rhs.d+rhs.n*lhs.d,lhs.d*rhs.d);
-}
-
-Fraction operator+=(Fraction& lhs, const Fraction& rhs) {
-    return lhs = Fraction(lhs.n*rhs.d+rhs.n*lhs.d,lhs.d*rhs.d);
-}
-
-Fraction operator-(const Fraction& lhs, const Fraction& rhs) {
-    return Fraction(lhs.n*rhs.d-rhs.n*lhs.d,lhs.d*rhs.d);
-}
-
-Fraction operator-=(Fraction& lhs, const Fraction& rhs) {
-    return lhs = Fraction(lhs.n*rhs.d-rhs.n*lhs.d,lhs.d*rhs.d);
-}
-
-Fraction operator*(const Fraction& lhs, const Fraction& rhs) {
-    return Fraction(lhs.n*rhs.n,lhs.d*rhs.d);
-}
-
-Fraction operator*=(Fraction& lhs, const Fraction& rhs) {
-    return lhs = Fraction(lhs.n*rhs.n,lhs.d*rhs.d);
-}
-
-Fraction operator*(int lhs, const Fraction& rhs) {
-    return Fraction(lhs*rhs.n,rhs.d);
-}
-
-Fraction operator*(const Fraction& rhs, int lhs) {
-    return Fraction(lhs*rhs.n,rhs.d);
-}
-
-Fraction operator/(const Fraction& lhs, const Fraction& rhs) {
-    return Fraction(lhs.n*rhs.d, lhs.d*rhs.n);
-}
+Fraction operator+=(Fraction& l, const Fraction& r) { return l = l+r; }
+Fraction operator-=(Fraction& l, const Fraction& r) { return l = l-r; }
+template<class T> Fraction operator*=(Fraction& l, const T& r) { return l = l*r; }
+template<class T> Fraction operator/=(Fraction& l, const T& r) { return l = l/r; }
 
 std::ostream& operator<<(std::ostream &strm, const Fraction &a) {
-    if (a.d == 1) {
-        strm << a.n;
-    } else {
-        strm << a.n << "/" << a.d;
-    }
+    strm << a.n;
+    if (a.d != 1) strm << "/" << a.d;
     return strm;
 }
--- Implementations/11 - Math (4)/11.1 - Number Theory/Mod.cpp
+++ Implementations/11 - Math (4)/11.1 - Number Theory/Mod.cpp
@@ -12,9 +12,14 @@ int mul(int a, int b) { return (ll)a*b%MOD; }
 pi operator+(const pi& l, const pi& r) { return {ad(l.f,r.f),ad(l.s,r.s)}; }
 pi operator-(const pi& l, const pi& r) { return {sub(l.f,r.f),sub(l.s,r.s)}; }
 pi operator*(const pi& l, const pi& r) { return {mul(l.f,r.f),mul(l.s,r.s)}; }
-pi operator*(const int& l, const pi& r) { return {mul(l,r.f),mul(l,r.s)}; }
-pi operator*(const pi& l, const int& r) { return r*l; }
+pi operator*(const pi& l, const int& r) { return l*pi(r,r); }
+pi operator*(const int& r, const pi& l) { return l*r; }
 
 pi operator+=(pi& l, const pi& r) { return l = l+r; }
 pi operator-=(pi& l, const pi& r) { return l = l-r; }
-pi operator*=(pi& l, const int& r) { return l = l*r; }
+template<class T> pi operator*=(pi& l, const T& r) { return l = l*r; }
+
+std::ostream& operator<<(std::ostream &strm, const pi& a) {
+    strm << a.f << " " << a.s << " | ";
+    return strm;
+}

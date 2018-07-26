d3af98c6ce2f709c57a254a0c3f3b83359768111
Benjamin Qi <bqi343@gmail.com>
2018-07-26 16:24:57 -0700

    Polynomials

--- Implementations/11 - Math (4)/11.1 - Number Theory/Mod.cpp
+++ Implementations/11 - Math (4)/11.1 - Number Theory/Mod.cpp
@@ -1,4 +1,4 @@
-/*
+/**
 * Description: Basic operations with modular arithmetic
 */
 
@@ -9,17 +9,6 @@ int ad(int a, int b) { return (a+b)%MOD; }
 int sub(int a, int b) { return (a-b+MOD)%MOD; }
 int mul(int a, int b) { return (ll)a*b%MOD; }
 
-pi operator+(const pi& l, const pi& r) { return {ad(l.f,r.f),ad(l.s,r.s)}; }
-pi operator-(const pi& l, const pi& r) { return {sub(l.f,r.f),sub(l.s,r.s)}; }
-pi operator*(const pi& l, const pi& r) { return {mul(l.f,r.f),mul(l.s,r.s)}; }
-pi operator*(const pi& l, const int& r) { return l*pi(r,r); }
-pi operator*(const int& r, const pi& l) { return l*r; }
-
-pi operator+=(pi& l, const pi& r) { return l = l+r; }
-pi operator-=(pi& l, const pi& r) { return l = l-r; }
-template<class T> pi operator*=(pi& l, const T& r) { return l = l*r; }
-
-std::ostream& operator<<(std::ostream &strm, const pi& a) {
-    strm << a.f << " " << a.s << " | ";
-    return strm;
-}
+int AD(int& a, int b) { return a = ad(a,b); }
+int SUB(int& a, int b) { return a = sub(a,b); }
+int MUL(int& a, int b) { return a = mul(a,b); }
--- Implementations/11 - Math (4)/Polynomials (6)/NTT.cpp
+++ Implementations/11 - Math (4)/Polynomials (6)/NTT.cpp
@@ -1,34 +1,30 @@
 /**
 * Description: Use if you are working with non-negative integers
-* Verification: http://codeforces.com/contest/632/submission/33953285
+* Usage: http://codeforces.com/contest/632/problem/E
+* Dependencies: vector, mod
 */
 
+const int MOD = (119 << 23) + 1, root = 3; // = 998244353
+// For p < 2^30 there is also e.g. (5 << 25, 3), (7 << 26, 3),
+// (479 << 21, 3) and (483 << 21, 5). The last two are > 10^9.
 
 namespace NTT {
-    const ll mod = (119 << 23) + 1, root = 3; // = 998244353
-    // For p < 2^30 there is also e.g. (5 << 25, 3), (7 << 26, 3),
-    // (479 << 21, 3) and (483 << 21, 5). The last two are > 10^9.
-        
-    ll modpow(ll b, ll p) { return !p?1:modpow(b*b%mod,p/2)*(p&1?b:1)%mod; }
-    
-    ll inv (ll b) { return modpow(b,mod-2); }
-    
     int get(int s) {
         return s > 1 ? 32 - __builtin_clz(s - 1) : 0;
     }
     
-    vl ntt(vl& a) { 
-        int n = a.size(), x = get(n); 
-        vl res, RES(n), roots(n);
-        roots[0] = 1, roots[1] = modpow(root,(mod-1)/n);
-        FOR(i,2,n) roots[i] = roots[i-1]*roots[1] % mod;
+    vi ntt(vi& a) { 
+        int n = sz(a), x = get(n); 
+        vi res, RES(n), roots(n);
+        roots[0] = 1, roots[1] = po(root,(MOD-1)/n);
+        FOR(i,2,n) roots[i] = mul(roots[i-1],roots[1]);
         
         res = a;
         FOR(i,1,x+1) {
             int inc = n>>i;
             F0R(j,inc) for (int k = 0; k < n; k += inc) {
                 int t = 2*k%n+j;
-                RES[k+j] = (res[t]+roots[k]*res[t+inc]) % mod;
+                RES[k+j] = ad(res[t],mul(roots[k],res[t+inc]));
             }
             swap(res,RES);
         }
@@ -36,31 +32,25 @@ namespace NTT {
         return res;
     }
     
-    vl ntt_rev(vl& a) {
-        vl res = ntt(a);
+    vi ntt_rev(vi& a) {
+        vi res = ntt(a);
         ll in = inv(sz(a));
-        F0R(i,sz(res)) res[i] = res[i]*in % mod;
+        F0R(i,sz(res)) MUL(res[i],in);
         reverse(res.begin() + 1, res.end());
         return res;
     }
     
-    vl brute(vl& a, vl& b) {
-        vl c(sz(a)+sz(b)-1);
-        F0R(i,sz(a)) F0R(j,sz(b)) c[i+j] = (c[i+j]+a[i]*b[j])%mod;
-        return c;
-    }
-    
-    vl conv(vl a, vl b) {
+    vi conv(vi a, vi b) {
         int s = sz(a)+sz(b)-1, L = get(s), n = 1<<L;
         if (s <= 0) return {};
-        if (s <= 200) return brute(a,b);
+        if (s <= 200) return a*b;
         
         a.resize(n); a = ntt(a);
         b.resize(n); b = ntt(b);
         
-        F0R(i,n) a[i] = a[i]*b[i] % mod;
-        a = ntt_rev(a);
+        F0R(i,n) MUL(a[i],b[i]);
         
+        a = ntt_rev(a);
         a.resize(s);
         return a;
     }

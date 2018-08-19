9559c158aafe91510264d3e32f8e36b96bdd68b4
Benjamin Qi <bqi343@gmail.com>
2018-08-19 09:30:35 -0400

    Update Map Customization.cpp

--- Implementations/05 - Data Structures (2)/5.2 - Set/Map Customization.cpp
+++ Implementations/05 - Data Structures (2)/5.2 - Set/Map Customization.cpp
@@ -3,18 +3,27 @@
 * Description: Define your own comparator / hash function
 */
 
+// map
+
 struct cmp {
     bool operator()(const int& l, const int& r) const {
         return l > r;
     }
 };
 
-struct hsh {
-    size_t operator()(const pi& k) const {
-        return k.f^k.s; // bad, but you get the point
+set<int,cmp> s;
+map<int,int,cmp> m;
+
+// unordered map
+
+int tmp = chrono::high_resolution_clock::now().time_since_epoch().count();
+
+template<class T> struct hsh {
+    size_t operator()(const T& x) const {
+        return hash<T>{}(x)^tmp;
     }
 };
 
-set<int,cmp> s;
-map<int,int,cmp> m;
-unordered_map<pi,int,hsh> u;
+template<class a, class b> using um = unordered_map<a,b,hsh<a>>;
+
+um<int,int> u;

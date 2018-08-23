aca955fd0108062596e8fecdb4ef991f92fc3878
Benjamin Qi <bqi343@gmail.com>
2018-08-23 15:35:18 -0400

    Update Unordered Map.cpp

--- Implementations/05 - Data Structures (2)/5.2 - Set/Unordered Map.cpp
+++ Implementations/05 - Data Structures (2)/5.2 - Set/Unordered Map.cpp
@@ -1,16 +1,23 @@
 /**
 * Description: faster than standard unordered map
 * Source: http://codeforces.com/blog/entry/60737
+* Verification: http://codeforces.com/contest/966/problem/E 
+    * normal unordered map gets TLE
 */
 
-const int tmp = chrono::high_resolution_clock::now().time_since_epoch().count();
+namespace mapOp {
+    const int tmp = chrono::high_resolution_clock::now().time_since_epoch().count();
 
-template<class T> struct hsh {
-    size_t operator()(const T& x) const { 
-        return hash<T>{}(x)^tmp; // avoid anti-hash tests?
+    template<class T> struct hsh {
+        size_t operator()(const T& x) const { 
+            return hash<T>{}(x)^tmp; // avoid anti-hash tests?
+        }
+    };
+
+    template<class a, class b> using um = gp_hash_table<a,b,hsh<a>>;
+
+    template<class a, class b> b get(um<a,b>& u, a x) {
+        if (u.find(x) == u.end()) return 0;
+        return u[x];
     }
-};
-
-template<class a, class b> using um = gp_hash_table<a,b,hsh<a>>;
-
-um<int,int> u;
+}

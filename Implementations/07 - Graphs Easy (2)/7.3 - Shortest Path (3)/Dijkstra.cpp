fd6be4ef31c3ea91fb5dfaf41d20c33a8f969f09
Benjamin Qi <bqi343@gmail.com>
2018-08-27 20:24:50 -0400

    Update Dijkstra.cpp

--- Implementations/07 - Graphs Easy (2)/7.3 - Shortest Path (3)/Dijkstra.cpp
+++ Implementations/07 - Graphs Easy (2)/7.3 - Shortest Path (3)/Dijkstra.cpp
@@ -3,10 +3,17 @@
 * Works with negative edge weights (aka SPFA?)
 */
 
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>;
+
+template<class T> T poll(pqg<T>& x) {
+    T y = x.top(); x.pop();
+    return y;
+}
+
 template<int SZ> struct Dijkstra {
     ll dist[SZ];
     vpi adj[SZ];
-    priority_queue<pl,vpl,greater<pl>> q;
+    pqg<pl> q;
     
     void addEdge(int A, int B, int C) {
         adj[A].pb({B,C}), adj[B].pb({A,C});
@@ -14,16 +21,12 @@ template<int SZ> struct Dijkstra {
     
     void gen(int st) {
         fill_n(dist,SZ,INF); 
-        q = priority_queue<pl,vpl,greater<pl>>();
-        
-        dist[st] = 0; q.push({0,st});
+        q = pqg<pl>(); q.push({dist[st] = 0,st});
     	while (sz(q)) {
-    		pl x = q.top(); q.pop();
+    		auto x = poll(q);
     		if (dist[x.s] < x.f) continue;
-    		for (pi y: adj[x.s]) if (x.f+y.s < dist[y.f]) {
-    			dist[y.f] = x.f+y.s;
-    			q.push({dist[y.f],y.f});
-    		}
+    		for (auto y: adj[x.s]) if (x.f+y.s < dist[y.f]) 
+    			q.push({dist[y.f] = x.f+y.s,y.f});
     	}
     }
 };

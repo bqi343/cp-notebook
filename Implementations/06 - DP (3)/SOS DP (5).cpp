d22f045df355edcc93f5f8276d0f5711d412fa4a
Benjamin Qi <bqi343@gmail.com>
2018-09-21 20:35:55 -0400

    Update SOS DP (5).cpp

--- Implementations/06 - DP (3)/SOS DP (5).cpp
+++ Implementations/06 - DP (3)/SOS DP (5).cpp
@@ -1,14 +1,10 @@
 /**
  * Description: if you add one to dp[i]
-    * it adds one to result[j] for all j such that j&i = j
+    * it adds one to dp[j] for all j such that j&i = j
  */
 
-vi sos (vi dp) {
-    int SZ = 31-__builtin_clz(sz(dp)); // sz(dp) = 1<<SZ
-    F0R(i,SZ) {
-        vi res = dp;
-        F0R(j,1<<SZ) if (j&(1<<i)) res[j^(1<<i)] += dp[j];
-        swap(res,dp);
-    }
-    return dp;
+void sos (vi& dp, int x = 1) { // x = -1 reverses
+    int SZ = 31-__builtin_clz(sz(dp));
+    F0R(i,SZ) F0R(j,1<<SZ) if (j&(1<<i)) 
+        dp[j^(1<<i)] += x*dp[j];
 }

2e2cf70e6014d92db1e2f42e337c8ca03aee3be9
Benjamin Qi <bqi343@gmail.com>
2018-10-09 15:57:14 -0400

    Update Treap.cpp

--- Implementations/09 - Range Queries (2)/15.3 - BBST (4)/Treap.cpp
+++ Implementations/09 - Range Queries (2)/15.3 - BBST (4)/Treap.cpp
@@ -1,97 +1,73 @@
 /*
-* Sources: various
+* Source: https://cp-algorithms.com/data_structures/treap.html + others
 * Description: Easiest BBST
 * Verification: http://www.spoj.com/problems/ORDERSET/
 */
 
-struct tnode {
-    int val, pri, sz;
-    tnode *c[2];
-
-    tnode (int v) {
-        val = v, sz = 1, pri = rand()+(rand()<<15);
-        c[0] = c[1] = NULL;
+namespace treap {
+    typedef struct tnode* pt;
+    
+    struct tnode {
+        int pri, val; pt c[2]; // essential
+        int sz; ll sum; // for range queries
+    
+        tnode (int _val) {
+            pri = rand()+(rand()<<15); val = _val; c[0] = c[1] = NULL;
+            sz = 1; sum = val;
+        }
+    };
+    
+    int getsz(pt x) { return x?x->sz:0; }
+    ll getsum(pt x) { return x?x->sum:0; }
+    
+    void trav(pt x, vi& v) {
+        if (!x) return;
+        trav(x->c[0],v); v.pb(x->val); trav(x->c[1],v);
     }
     
-    void inOrder(bool f = 0) {
-        if (c[0]) c[0]->inOrder();
-        cout << val << " ";
-        if (c[1]) c[1]->inOrder();
-        if (f) cout << "\n------------\n";
+    pt recalc(pt x) {
+        x->sz = 1+getsz(x->c[0])+getsz(x->c[1]);
+        x->sum = x->val+getsum(x->c[0])+getsum(x->c[1]);
+        return x;
     }
     
-    void recalc() {
-        sz = 1+(c[0]?c[0]->sz:0)+(c[1]?c[1]->sz:0);
+    pair<pt,pt> split(pt t, int v) { // >= v goes to the right
+        if (!t) return {t,t};
+        if (t->val >= v) {
+            auto p = split(t->c[0], v); t->c[0] = p.s;
+            return {p.f, recalc(t)};
+        } else {
+            auto p = split(t->c[1], v); t->c[1] = p.f;
+            return {recalc(t), p.s};
+        }
     }
-};
-
-pair<tnode*,tnode*> split(tnode* t, int v) { // >= v goes to the right
-    if (!t) return {t,t};
     
-    if (v <= t->val) {
-        auto p = split(t->c[0], v); 
-        t->c[0] = p.s; t->recalc();
-        return {p.f, t};
-    } else {
-        auto p = split(t->c[1], v); 
-        t->c[1] = p.f; t->recalc();
-        return {t, p.s};
+    pair<pt,pt> splitsz(pt t, int sz) {
+        if (!t) return {t,t};
+        if (getsz(t->c[0]) >= sz) {
+            auto p = splitsz(t->c[0], sz); t->c[0] = p.s;
+            return {p.f, recalc(t)};
+        } else {
+            auto p = splitsz(t->c[1], sz-getsz(t->c[0])-1); t->c[1] = p.f;
+            return {recalc(t), p.s};
+        }
     }
-}
+        
+    pt merge(pt l, pt r) {
+        if (!l || !r) return l ? l : r;
+        pt t;
+        if (l->pri > r->pri) l->c[1] = merge(l->c[1],r), t = l;
+        else r->c[0] = merge(l,r->c[0]), t = r;
+        return recalc(t);
+    }
     
-pair<tnode*,tnode*> split_by_order(tnode* t, int v) {
-    if (!t) return {t,t};
-    int tmp = t->c[0]?t->c[0]->sz:0;
-    if (v <= tmp) {
-        auto p = split_by_order(t->c[0], v);
-        t->c[0] = p.s; t->recalc();
-        return {p.f, t};
-    } else {
-        auto p = split_by_order(t->c[1], v-tmp-1); 
-        t->c[1] = p.f; t->recalc();
-        return {t, p.s};
+    pt ins(pt x, int v) { // insert v
+        auto a = split(x,v), b = split(a.s,v+1);
+        return merge(a.f,merge(new tnode(v),b.s));
     }
-}
-
-tnode* merge(tnode* l, tnode* r) {
-    if (!l) return r; 
-    if (!r) return l;
     
-    if (l->pri > r->pri) {
-        l->c[1] = merge(l->c[1],r);
-        l->recalc();
-        return l;
-    } else {
-        r->c[0] = merge(l,r->c[0]);
-        r->recalc();
-        return r;
+    pt del(pt x, int v) { // delete v
+        auto a = split(x,v), b = split(a.s,v+1);
+        return merge(a.f,b.s);
     }
-}
-
-tnode* ins(tnode* x, int v) { // insert value v
-    auto a = split(x,v);
-    auto b = split(a.s,v+1);
-    return merge(a.f,merge(new tnode(v),b.s));
-}
-
-tnode* del(tnode* x, int v) { // delete all values equal to v
-    auto a = split(x,v), b = split(a.s,v+1);
-    return merge(a.f,b.s);
-}
-
-tnode *root;
-
-int order_of_key(int x) {
-    auto a = split(root,x);
-    int t = a.f?a.f->sz:0;
-    root = merge(a.f,a.s);
-    return t;
-}
-
-int find_by_order(int x) {
-    auto a = split_by_order(root,x);
-    auto b = split_by_order(a.f,x-1);
-    int t = b.s->val;
-    root = merge(merge(b.f,b.s),a.s);
-    return t;
 }

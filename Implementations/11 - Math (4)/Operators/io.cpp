9aef7753d4bdd4a2350ed40239c68b92f3260b28
Benjamin Qi <bqi343@gmail.com>
2018-10-12 11:00:37 -0400

    Update io.cpp

--- Implementations/11 - Math (4)/Operators/io.cpp
+++ Implementations/11 - Math (4)/Operators/io.cpp
@@ -1,5 +1,6 @@
 /**
-* Description: experimentation with C++11 features
+* Description: more convenient functions for input / output
+  * experimentation with C++11 features
 */
 
 namespace io {
@@ -10,37 +11,46 @@ namespace io {
         if (sz(s)) { setIn(s+".in"), setOut(s+".out"); }
     }
     
+    // INPUT 
+    
     // double input seems slow on CF
     void re(double& x) { string t; cin >> t; x = stod(t); } 
     void re(ld& x) { string t; cin >> t; x = stold(t); }
     template<class T> void re(T& x) { cin >> x; }
-    template<class Arg, class... Args> void re(Arg& first, Args&... rest) { re(first); re(rest...); }
+    template<class Arg, class... Args> void re(Arg& first, Args&... rest) { 
+        re(first); 
+        re(rest...); 
+    }
+    template<class T1, class T2> istream& operator>>(istream& is, pair<T1,T2>& p) {
+        is >> p.f >> p.s;
+        return is;
+    }
+    template<class T> istream& operator>>(istream& is, vector<T>& a) {
+        F0R(i,sz(a)) is >> a[i];
+        return is;
+    }
     
-	// pair
-	template<class T1, class T2> istream& operator>>(istream& is, pair<T1,T2>& p) {
-	    is >> p.f >> p.s;
-	    return is;
-	}
-	template<class T1, class T2> ostream& operator<<(ostream& os, const pair<T1,T2>& a) {
-	    os << '{' << a.f << ", " << a.s << '}';
-	    return os;
-	}
-	
-    // vector
-	template<class T> istream& operator>>(istream& is, vector<T>& a) {
-	    int sz; is >> sz; a.resize(sz);
-	    F0R(i,sz) is >> a[i];
-	    return is;
-	}
-	template<class T> ostream& operator<<(ostream& os, const vector<T>& a) {
-	    os << "{";
-	    F0R(i,sz(a)) {
-	        if (i) os << ", ";
-	        os << a[i];
-	    }
-	    os << "}";
-	    return os;
-	}
+    // OUTPUT 
+    
+    template<class T> void pr(T& x) { cout << x; }
+    template<class Arg, class... Args> void pr(Arg& first, Args&... rest) { 
+        pr(first); 
+        cout << ' ';
+        pr(rest...); 
+    }
+    template<class T1, class T2> ostream& operator<<(ostream& os, const pair<T1,T2>& a) {
+        os << '{' << a.f << ", " << a.s << '}';
+        return os;
+    }
+    template<class T> ostream& operator<<(ostream& os, const vector<T>& a) {
+        os << '{';
+        F0R(i,sz(a)) {
+            if (i) os << ", ";
+            os << a[i];
+        }
+        os << '}';
+        return os;
+    }
 }
 
 using namespace io;

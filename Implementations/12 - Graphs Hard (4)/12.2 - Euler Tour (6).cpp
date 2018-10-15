7adbf87905ebc299ca7480b1af3d734d94cdb859
Benjamin Qi <bqi343@gmail.com>
2018-10-15 11:16:38 -0400

    Update 12.2 - Euler Tour (6).cpp

--- Implementations/12 - Graphs Hard (4)/12.2 - Euler Tour (6).cpp
+++ Implementations/12 - Graphs Hard (4)/12.2 - Euler Tour (6).cpp
@@ -1,49 +1,59 @@
 /**
-* Description: extra log factor (remove this ...)
+* Description: O(N+M) euler tour for both directed and undirected graphs
 * Source: USACO Training
-* Verification: https://open.kattis.com/problems/eulerianpath
+* Verification: 
+    * directed: https://open.kattis.com/problems/eulerianpath
+    * undirected: USACO Training 3.3, Riding the Fences
 */
 
 struct Euler {
-    vi circuit;
-    multiset<int> adj[MX], ADJ[MX];
-    int N,M, out[MX], in[MX];
+    vpi adj[MX], circuit;
+    int N, M, nex, out[MX], in[MX], deg[MX];
+    bool used[MX], bad;
     
-    void find_circuit(int x) { // directed graph, possible that resulting circuit is not valid
-        while (sz(adj[x])) {
-            int j = *adj[x].begin(); adj[x].erase(adj[x].begin());
-            find_circuit(j);
-        }
-        circuit.pb(x);
+    void clr() {
+        F0R(i,N) adj[i].clear();
+        circuit.clear(); 
+        nex = 0;
+        F0R(i,N) out[i] = in[i] = deg[i];
+        F0R(i,M) used[i] = 0;
+        bad = 0;
     }
     
-    int a,b,start; 
+    void find_circuit(int pre, int cur) { 
+        while (sz(adj[cur])) {
+            pi x = adj[cur].back(); adj[cur].pop_back();
+            if (used[x.s]) continue;
+            used[x.s] = 1; 
+            find_circuit(cur,x.f);
+        }
+        if (sz(circuit) && circuit.back().f != cur) bad = 1;
+        circuit.pb({pre,cur});
+    }
     
-    vi solve() {
-        F0R(i,N) {
-            adj[i].clear(), ADJ[i].clear();
-            out[i] = in[i] = 0;
+    void addEdge(int a, int b, bool directed) {
+        if (directed) {
+            adj[a].pb({b,nex}); 
+            out[a] ++, in[b] ++; nex ++;
+        } else {
+            adj[a].pb({b,nex}), adj[b].pb({a,nex});
+            deg[a] ++, deg[b] ++; nex ++;
         }
-        circuit.clear();
-        F0R(i,M) {
-            cin >> a >> b; // add edges
-            adj[a].insert(b), ADJ[a].insert(b);
-            out[a] ++, in[b] ++;
-        }
-        start = a;
-        F0R(i,N) if (out[i]-in[i] == 1) start = i;
+    }
+    
+    vi solve(bool directed) { // edges only involve vertices from 0 to N-1
+        int start = 0;
+        F0R(i,N) if (deg[i] || in[i] || out[i]) start = i;
         
-        find_circuit(start);
-        reverse(all(circuit));
-        
-        if (sz(circuit) != M+1) return {};
-        
-        F0R(i,M) { // verify that circuit is valid
-            if (ADJ[circuit[i]].find(circuit[i+1]) == ADJ[circuit[i]].end()) return {};
-            int t = circuit[i];
-            ADJ[t].erase(ADJ[t].find(circuit[i+1]));
+        if (directed) {
+            F0R(i,N) if (out[i]-in[i] == 1) start = i;
+        } else {
+            F0R(i,N) if (deg[i] % 2 == 1) start = i;
         }
         
-        return circuit;
+        find_circuit(-1,start);
+        if (sz(circuit) != M+1 || bad) return {};
+        vi ans; F0Rd(i,sz(circuit)) ans.pb(circuit[i].s);
+        return ans;
     }
 };

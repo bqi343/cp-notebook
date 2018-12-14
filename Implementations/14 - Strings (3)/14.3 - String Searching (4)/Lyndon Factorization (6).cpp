07841d8db948517ff12c64ad3ddd3738f84a4bad
Benjamin Qi <bqi343@gmail.com>
2018-12-14 10:31:20 -0500

    Strings

---
+++ Implementations/14 - Strings (3)/14.3 - String Searching (4)/Lyndon Factorization (6).cpp
@@ -0,0 +1,22 @@
+/**
+ * Description: Compute Lyndon Factorization in O(n)
+ * Source: https://cp-algorithms.com/string/lyndon_factorization.html
+ * Verification: https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=660
+ */
+ 
+vector<string> duval(string const& s) {
+    int n = s.size();
+    vector<string> factorization;
+    for (int i = 0; i < n; ) {
+        int j = i + 1, k = i;
+        for (;j < n && s[k] <= s[j]; ++j) {
+            if (s[k] < s[j]) k = i;
+            else k++;
+        }
+        while (i <= k) {
+            factorization.pb(s.substr(i, j - k));
+            i += j - k;
+        }
+    }
+    return factorization;
+}

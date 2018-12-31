1e656086f2e4c287fb23def79ceeed5cfa385a77
Benq <bqi343@gmail.com>
2018-12-31 14:26:00 -0500

    geo

---
+++ Implementations/13 - Geometry (4)/13.1 - Techniques/Point.cpp
@@ -0,0 +1,80 @@
+/**
+* Description: Easy Geo
+* Source: http://codeforces.com/blog/entry/22175, KACTL
+* Verification: various
+*/
+
+typedef pd P;
+
+namespace point {
+    typedef vector<P> vP;
+
+    ld norm(P x) { return x.f*x.f+x.s*x.s; }
+    ld abs(P x) { return sqrt(norm(x)); }
+    ld angle(P x) { return atan2(x.s,x.f); }
+    P conj(P x) { return P(x.f,-x.s); }
+    
+    P operator+(const P& l, const P& r) { return P(l.f+r.f,l.s+r.s); }
+    P operator-(const P& l, const P& r) { return P(l.f-r.f,l.s-r.s); }
+    P operator*(const P& l, const ld& r) { return P(l.f*r,l.s*r); }
+    P operator*(const ld& l, const P& r) { return r*l; }
+    P operator/(const P& l, const ld& r) { return P(l.f/r,l.s/r); }
+    
+    P operator*(const P& l, const P& r) { return P(l.f*r.f-l.s*r.s,l.s*r.f+l.f*r.s); }
+    P operator/(const P& l, const P& r) { return l*conj(r)/norm(r); }
+    
+    template<class T> T operator += (T& l, const T& r) { return l = l+r; }
+    template<class T> T operator -= (T& l, const T& r) { return l = l-r; }
+    template<class T> T operator *= (T& l, const T& r) { return l = l*r; }
+    template<class T> T operator /= (T& l, const T& r) { return l = l/r; }
+    
+    ld dot(P a, P b) { return (conj(a)*b).f; }
+    ld cross(P a, P b) { return (conj(a)*b).s; }
+    ld cross(P p, P a, P b) { return cross(a-p,b-p); }
+    ld dist(P p, P a, P b) { return std::abs(cross(p,a,b))/abs(a-b); }
+    
+    P rotate(P a, ld b) { return a*P(cos(b),sin(b)); }
+    P reflect(P p, P a, P b) { return a+conj((p-a)/(b-a))*(b-a); }
+    P foot(P p, P a, P b) { return (p+reflect(p,a,b))/(ld)2; }
+    P extension(P a, P b, P c, P d) {
+        ld x = cross(a,b,c), y = cross(a,b,d);
+        return (d*x-c*y)/(x-y);
+    }
+    
+    // sorts points according to atan2
+    // verification: ?
+    template<class T> int half(pair<T,T> x) { return mp(x.s,x.f) > mp((T)0,(T)0); }
+    bool cmp(P a, P b) { 
+        int A = half(a), B = half(b);
+        if (A != B) return A < B;
+        return cross(a,b) > 0;
+    }
+    
+    // computes the center of mass of a polygon with constant mass per unit area
+    // verification: kattis polygonarea, VT HSPC 2018 Holiday Stars
+    P centroid(vP v) { 
+        P cen(0,0); ld area = 0;
+        F0R(i,sz(v)) {
+            int j = (i+1)%sz(v);
+            ld a = cross(v[i],v[j]);
+            cen += a*(v[i]+v[j]); area += a;
+        }
+        area /= (ld)2; // positive if ccw
+        return cen/area/(ld)6;
+    }
+
+    // tests whether a point is inside, on, or outside the perimeter of any polygon
+    // verification: https://open.kattis.com/problems/pointinpolygon
+    string inPoly(vP p, P z) {
+        int n = sz(p), ans = 0;
+        F0R(i,n) {
+            P x = p[i], y = p[(i+1)%n];
+            if (cross(x,y,z) == 0 && min(x,y) <= z && z <= max(x,y)) return "on";
+            if (x.s > y.s) swap(x,y);
+            if (x.s <= z.s && y.s > z.s && cross(z,x,y) > 0) ans ^= 1;
+        }
+        return ans ? "in" : "out";
+    }
+};
+
+using namespace point;
--- Implementations/13 - Geometry (4)/13.2 - Sweep Line/Convex Hull.cpp
+++ Implementations/13 - Geometry (4)/13.2 - Sweep Line/Convex Hull.cpp
@@ -4,24 +4,23 @@
 * Verification: https://open.kattis.com/problems/convexhull
 */
 
-ll cross(pi O, pi A, pi B) {
-    return (ll)(A.f-O.f)*(B.s-O.s)
-            -(ll)(A.s-O.s)*(B.f-O.f);
-}
+typedef pl P;
 
-vpi convex_hull(vpi P) {
+using namespace point;
+
+vP convex_hull(vP P) {
     sort(all(P)); P.erase(unique(all(P)),P.end());
     int n = sz(P);
     if (n == 1) return P;
     
-    vpi bot = {P[0]};
+    vP bot = {P[0]};
     FOR(i,1,n) {
         while (sz(bot) > 1 && cross(bot[sz(bot)-2], bot.back(), P[i]) <= 0) bot.pop_back();
         bot.pb(P[i]);
     }
     bot.pop_back();
     
-    vpi up = {P[n-1]};
+    vP up = {P[n-1]};
     F0Rd(i,n-1) {
         while (sz(up) > 1 && cross(up[sz(up)-2], up.back(), P[i]) <= 0) up.pop_back();
         up.pb(P[i]);

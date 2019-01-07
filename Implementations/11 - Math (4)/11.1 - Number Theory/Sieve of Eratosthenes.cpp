3bb1820a215e169458e7540ffd83dd9ccc9c9afa
Benjamin Qi <bqi343@gmail.com>
2019-01-07 11:44:47 -0500

    fix indices

--- Implementations/11 - Math (4)/11.1 - Number Theory/Sieve of Eratosthenes.cpp
+++ Implementations/11 - Math (4)/11.1 - Number Theory/Sieve of Eratosthenes.cpp
@@ -1,17 +1,17 @@
 /**
 * Description: Tests primality up to n in O(nlog(logn)) 
-* Source: ?
+* Source: KACTL?
 * Verification: https://open.kattis.com/problems/primesieve
 */
 
-template<int SZ> struct sieve { 
+template<int SZ> struct Sieve { 
 	bitset<SZ> comp;
 	vi pr;
 	// int sp[SZ];
 
-	sieve() {
-		for (int i = 2; i*i <= SZ; ++i) if (!comp[i]) 
-			for (int j = i*i; j <= SZ; j += i) comp[j] = 1;
+	Sieve() {
+		for (int i = 2; i*i < SZ; ++i) if (!comp[i]) 
+			for (int j = i*i; j < SZ; j += i) comp[j] = 1;
 		FOR(i,2,SZ) if (!comp[i]) pr.pb(i);
 		
 		/*FOR(i,2,SZ) { // O(N) sieve

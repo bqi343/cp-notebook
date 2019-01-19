a3c369f3a5c00e4c91074af07d2478aa556c2648
Benjamin Qi <bqi343@gmail.com>
2019-01-19 12:24:04 -0500

    16 Jan G1

--- Contests/USACO Solutions/2015-16/Jan 2016/Gold/16 Jan G1.cpp
+++ Contests/USACO Solutions/2015-16/Jan 2016/Gold/16 Jan G1.cpp
@@ -1,70 +1,200 @@
-//#include<iostream>
-#include<fstream>
-#include<vector>
-#include<algorithm>
-#include<iomanip>
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/rope>
 
 using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
  
 typedef long long ll;
-typedef vector<int> vi;
+typedef long double ld;
+typedef complex<ld> cd;
 
-#define FOR(i, a, b) for (int i=a; i<b; i++)
-#define F0R(i, a) for (int i=0; i<a; i++)
- 
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= (a); i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a, x) for (auto& a : x)
+
 #define mp make_pair
 #define pb push_back
+#define f first
+#define s second
 #define lb lower_bound
 #define ub upper_bound
 
-vi pos;
-double ans = 1000000000;
-int n1[50000], n2[50000], N;
+#define sz(x) (int)x.size()
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) beg(x), en(x)
+#define resz resize
 
-bool great(int rad) {
-	int low = 0, high = N-1;
-	while (low<N-1 && n1[low+1] <= rad) low++;
-	while (high>0 && n2[high-1] <= rad) high--;
-	double mid = double(pos[high]-pos[low])/2;
-	ans = min(ans,max((double)rad+1,mid));
-	if (rad+1<mid) return 0;
-	return 1;
+const int MOD = 1000000007; // 998244353
+const ll INF = 1e18;
+const int MX = 100001;
+const ld PI = 4*atan((ld)1);
+
+template<class T> void ckmin(T &a, T b) { a = min(a, b); }
+template<class T> void ckmax(T &a, T b) { a = max(a, b); }
+
+namespace io {
+    // TYPE ID (StackOverflow)
+    
+    template<class T> struct like_array : is_array<T>{};
+    template<class T, size_t N> struct like_array<array<T,N>> : true_type{};
+    template<class T> struct like_array<vector<T>> : true_type{};
+    template<class T> bool is_like_array(const T& a) { return like_array<T>::value; }
+
+    // I/O 
+    
+    void setIn(string s) { freopen(s.c_str(),"r",stdin); }
+    void setOut(string s) { freopen(s.c_str(),"w",stdout); }
+    void setIO(string s = "") {
+        ios_base::sync_with_stdio(0); cin.tie(0);
+        if (sz(s)) { setIn(s+".in"), setOut(s+".out"); }
+    }
+    
+    // INPUT 
+    
+    template<class T> void re(T& x) { cin >> x; }
+    template<class Arg, class... Args> void re(Arg& first, Args&... rest);
+    void re(double& x) { string t; re(t); x = stod(t); }
+    void re(ld& x) { string t; re(t); x = stold(t); }
+    
+    template<class T> void re(complex<T>& x);
+    template<class T1, class T2> void re(pair<T1,T2>& p);
+    template<class T> void re(vector<T>& a);
+    template<class T, size_t SZ> void re(array<T,SZ>& a);
+    
+    template<class Arg, class... Args> void re(Arg& first, Args&... rest) { re(first); re(rest...); }
+    template<class T> void re(complex<T>& x) { T a,b; re(a,b); x = cd(a,b); }
+    template<class T1, class T2> void re(pair<T1,T2>& p) { re(p.f,p.s); }
+    template<class T> void re(vector<T>& a) { F0R(i,sz(a)) re(a[i]); }
+    template<class T, size_t SZ> void re(array<T,SZ>& a) { F0R(i,SZ) re(a[i]); }
+    
+    // OUTPUT 
+    
+    template<class T1, class T2> ostream& operator<<(ostream& os, const pair<T1,T2>& a) {
+        os << '{' << a.f << ", " << a.s << '}'; return os;
+    }
+    template<class T> ostream& printArray(ostream& os, const T& a, int SZ) {
+        os << '{';
+        F0R(i,SZ) {
+            if (i) {
+                os << ", ";
+                if (is_like_array(a[i])) cout << "\n";
+            }
+            os << a[i];
+        }
+        os << '}';
+        return os;
+    }
+    template<class T, size_t SZ> ostream& operator<<(ostream& os, const array<T,SZ>& a) {
+        return printArray(os,a,SZ);
+    }
+    template<class T> ostream& operator<<(ostream& os, const vector<T>& a) {
+        return printArray(os,a,sz(a));
+    }
+    template<class T> ostream& operator<<(ostream& os, const set<T>& a) {
+        os << vector<T>(all(a)); return os;
+    }
+    template<class T1, class T2> ostream& operator<<(ostream& os, const map<T1,T2>& a) {
+        os << vector<pair<T1,T2>>(all(a)); return os;
+    }
+    
+    template<class T> void pr(const T& x) { cout << x << '\n'; }
+    template<class Arg, class... Args> void pr(const Arg& first, const Args&... rest) { 
+        cout << first << ' '; pr(rest...); 
+    }
+}
+
+using namespace io;
+
+namespace modOp {
+    int ad(int a, int b, int mod = MOD) { return (a+b)%mod; }
+    int sub(int a, int b, int mod = MOD) { return (a-b+mod)%mod; }
+    int mul(int a, int b, int mod = MOD) { return (ll)a*b%mod; }
+    
+    int AD(int& a, int b, int mod = MOD) { return a = ad(a,b,mod); }
+    int SUB(int& a, int b, int mod = MOD) { return a = sub(a,b,mod); }
+    int MUL(int& a, int b, int mod = MOD) { return a = mul(a,b,mod); }
+    
+    int po (int b, int p, int mod = MOD) { return !p?1:mul(po(mul(b,b,mod),p/2,mod),p&1?b:1,mod); }
+    int inv (int b, int mod = MOD) { return po(b,mod-2,mod); }
+    
+    int invGeneral(int a, int b) { // 0 < a < b, gcd(a,b) = 1
+        if (a == 0) return b == 1 ? 0 : -1;
+        int x = invGeneral(b%a,a); 
+        return x == -1 ? -1 : ((1-(ll)b*x)/a+b)%b;
+    }
+}
+
+using namespace modOp;
+
+int N;
+vi need[2];
+vi v;
+
+vi calc(vi v) {
+    vi res(N);
+    int ind = 0;
+    FOR(i,1,N) {
+        res[i] = MOD;
+        // max(v[i]-v[ind],need[0][ind]+1)
+        while (v[i]-v[ind+1] > res[ind+1]) ind ++;
+        res[i] = min(v[i]-v[ind],res[ind+1]+1);
+    }
+    return res;
+}
+
+vi trans(vi v) {
+    trav(i,v) i *= -1;
+    reverse(all(v));
+    return v;
 }
 
 int main() {
-	ifstream cin ("angry.in");
-	ofstream cout ("angry.out");
-	cin >> N; pos.resize(N);
-	F0R(i,N) cin >> pos[i];
-	sort(pos.begin(),pos.end());
-	FOR(i,1,N) {
-		if (pos[i]-pos[i-1]>n1[i-1]) {
-			n1[i] = pos[i]-pos[i-1];
-			continue;
-		}
-		auto x = lb(pos.begin(),pos.end(),pos[i]-n1[i-1]);
-		int ind = distance(pos.begin(),x);
-		if (n1[ind] >= n1[i-1]) n1[i] = n1[i-1]+1;
-		else n1[i] = n1[i-1];
-		//if (n1[i]<n1[i-1]) cout << n1[i] << "\n";
-	}
-	for (int i = N-2; i >= 0; --i) {
-		if ((pos[i+1]-pos[i])>n2[i+1]) {
-			n2[i] = pos[i+1]-pos[i];
-			continue;
-		}
-		auto x = prev(ub(pos.begin(),pos.end(),pos[i]+n2[i+1]));
-		int ind = distance(pos.begin(),x);
-		if (n2[ind] >= n2[i+1]) n2[i] = n2[i+1]+1;
-		else n2[i] = n2[i+1];
-		//cout << n2[i] << " ";
-	}
-	F0R(i,N) ans = min(ans,(double)max(n1[i],n2[i]));
-	int low = 0, high = 1000000000;
-	while (low<high) {
-		int mid = (low+high)/2;
-		if (great(mid)) high = mid;
-		else low = mid+1;
-	}
-	cout << fixed << setprecision(1) << ans;
+    // you should actually read the stuff at the bottom
+    setIO("angry"); 
+    re(N); v.resz(N); re(v); sort(all(v));
+    need[0] = calc(v);
+    vi V = trans(v); need[1] = calc(V); reverse(all(need[1]));
+    
+    // pr(v,need[0],need[1]);
+    int lo = 0, hi = sz(v)-1, R = 0;
+    ld ans = INF;
+    while (1) {
+        ans = min(ans,max(R+(ld)1,(v[hi]-v[lo])/(ld)2));
+        if (lo == hi) break;
+        if (need[0][lo+1] < need[1][hi-1]) R = need[0][++lo];
+        else R = need[1][--hi];
+    }
+    cout << fixed << setprecision(1) << ans;
+    // for R: find the last one in need[0] <= R 
+    // find the first one in need[1] <= R 
+    // max(R+1,dif/2);
+    // you should actually read the stuff at the bottom
 }
+
+/* stuff you should look for
+    * int overflow, array bounds
+    * special cases (n=1?), set tle
+    * do smth instead of nothing and stay organized
+*/

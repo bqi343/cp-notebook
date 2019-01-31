757659100c8726d0c16d9164bbca3e1298a8bb46
Benjamin Qi <bqi343@gmail.com>
2019-01-30 22:06:19 -0500

    BIT

--- Implementations/09 - Range Queries (2)/09.2 - 1D Range Queries (3)/BIT.cpp
+++ Implementations/09 - Range Queries (2)/09.2 - 1D Range Queries (3)/BIT.cpp
@@ -1,22 +1,36 @@
 /**
-* Description: 1D range sum query with point update
-* Source: TopCoder
-* Verification: SPOJ Fenwick
-*/
+ * Description: N-D range sum query with point update
+ * Source: https://codeforces.com/blog/entry/64914
+ * Verification: SPOJ matsum
+ */
 
-template<class T, int SZ> struct BIT {
-    T bit[SZ+1];
-    
-    BIT() { memset(bit,0,sizeof bit); }
-    
-    void upd(int k, T val) { // add val to index k
-        for( ;k <= SZ; k += (k&-k)) bit[k] += val;
-    }
-    
-    T query(int k) {
-        T temp = 0;
-        for (;k > 0;k -= (k&-k)) temp += bit[k];
-        return temp;
-    }
-    T query(int l, int r) { return query(r)-query(l-1); } // range query [l,r]
+template <class T, int ...Ns> struct BIT {
+    T val = 0;
+    void upd(T v) { val += v; }
+    T query() { return val; }
 };
+
+template <class T, int N, int... Ns> struct BIT<T, N, Ns...> {
+    BIT<T,Ns...> bit[N + 1];
+    template<typename... Args> void upd(int pos, Args... args) {
+        for (; pos <= N; pos += (pos&-pos)) bit[pos].upd(args...);
+    }
+    template<typename... Args> T sum(int r, Args... args) {
+        T res = 0; for (; r; r -= (r&-r)) res += bit[r].query(args...); 
+        return res;
+    }
+    template<typename... Args> T query(int l, int r, Args... args) {
+        return sum(r,args...)-sum(l-1,args...);
+    }
+}; // BIT<int,10,10> gives a 2D BIT
+
+template<class T, int SZ> struct BITrange {
+    BIT<T,SZ> bit[2]; // piecewise linear functions
+    void upd(int hi, T val) {
+        bit[1].upd(1,val), bit[1].upd(hi+1,-val);
+        bit[0].upd(hi+1,hi*val);
+    }
+    void upd(int lo, int hi, T val) { upd(lo-1,-val), upd(hi,val); }
+    T sum(int x) { return bit[1].sum(x)*x+bit[0].sum(x); }
+    T query(int x, int y) { return sum(y)-sum(x-1); }
+}; // equivalent to 1D lazy segment tree for sum

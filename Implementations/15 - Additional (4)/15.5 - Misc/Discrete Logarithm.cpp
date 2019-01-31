8a0d992c8b41b303e728a3dc87db77c82c2a3034
Benjamin Qi <bqi343@gmail.com>
2019-01-31 14:39:56 -0500

    Update and rename discreteLog.cpp to Discrete Logarithm.cpp

--- Implementations/15 - Additional (4)/15.5 - Misc/Discrete Logarithm.cpp
+++ Implementations/15 - Additional (4)/15.5 - Misc/Discrete Logarithm.cpp
@@ -1,6 +1,7 @@
 /**
- * Description: find k such that primitive^k=x
- 	* meet in the middle: O(sqrt(MOD))
+ * Description: find k such that root^k%mod=x
+ 	* mod is prime, root is primitive
+ 	* meet in the middle: O(sqrt(mod))
  * Source: Own
  * Verification: PA 2006 - Professor Laugh's Numbers
  */
@@ -8,8 +9,8 @@
 using namespace modOp;
 
 struct DiscreteLog {
-    int mod, root, block;
-    vi invy;
+	int mod, root, block;
+	vi invy;
 	unordered_map<int,int> u;
 
 	int query(int x) {
@@ -21,14 +22,14 @@ struct DiscreteLog {
 	}
 
 	void gen(int _mod, int _root) {
-	    mod = _mod, root = _root, u.clear();
-		
+		mod = _mod, root = _root, u.clear();
+
 		block = sqrt(mod)+1;
-	    int cur = 1;
+		int cur = 1;
 		F0R(i,block) u[cur] = i, MUL(cur,root,mod);
 		cur = inv(cur,mod);
 
-        invy.resize(block);
-        invy[0] = 1; FOR(i,1,block) invy[i] = mul(cur,invy[i-1],mod);
+		invy.resz(block);
+		invy[0] = 1; FOR(i,1,block) invy[i] = mul(cur,invy[i-1],mod);
 	} 
 };

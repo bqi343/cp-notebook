620c164917fb2508f65e79bdc6cc961c6b4a7b17
Benjamin Qi <bqi343@gmail.com>
2019-02-17 23:19:20 -0500

    Update C++ Input & Output.cpp

--- Implementations/00 - Contest/C++ Input & Output.cpp
+++ Implementations/00 - Contest/C++ Input & Output.cpp
@@ -1,77 +1,69 @@
 /**
-* Description: more convenient functions for input / output
-* Source: Misc
-* Verification: http://codeforces.com/contest/1045/problem/D
-*/
+ * Description: convenient functions for input / output
+ * Source: https://codeforces.com/blog/entry/65311, misc others
+ * Verification: http://codeforces.com/contest/1045/problem/D
+ */
 
-namespace io {
-    // TYPE ID (StackOverflow)
-    
-    template<class T> struct like_array : is_array<T>{};
-    template<class T, size_t N> struct like_array<array<T,N>> : true_type{};
-    template<class T> struct like_array<vector<T>> : true_type{};
-    template<class T> bool is_like_array(const T& a) { return like_array<T>::value; }
-
-    // I/O 
-    
-    void setIn(string s) { freopen(s.c_str(),"r",stdin); }
-    void setOut(string s) { freopen(s.c_str(),"w",stdout); }
-    void setIO(string s = "") {
-        ios_base::sync_with_stdio(0); cin.tie(0);
-        if (sz(s)) { setIn(s+".in"), setOut(s+".out"); }
-    }
-    
-    // INPUT 
-    
-    template<class T> void re(T& x) { cin >> x; }
-    template<class Arg, class... Args> void re(Arg& first, Args&... rest);
-    void re(double& x) { string t; re(t); x = stod(t); }
-    void re(ld& x) { string t; re(t); x = stold(t); }
-    
+namespace input {
     template<class T> void re(complex<T>& x);
     template<class T1, class T2> void re(pair<T1,T2>& p);
     template<class T> void re(vector<T>& a);
     template<class T, size_t SZ> void re(array<T,SZ>& a);
-    
-    template<class Arg, class... Args> void re(Arg& first, Args&... rest) { re(first); re(rest...); }
+
+    template<class T> void re(T& x) { cin >> x; }
+    void re(double& x) { string t; re(t); x = stod(t); }
+    void re(ld& x) { string t; re(t); x = stold(t); }
+    template<class Arg, class... Args> void re(Arg& first, Args&... rest) { 
+        re(first); re(rest...); 
+    }
+
     template<class T> void re(complex<T>& x) { T a,b; re(a,b); x = cd(a,b); }
     template<class T1, class T2> void re(pair<T1,T2>& p) { re(p.f,p.s); }
     template<class T> void re(vector<T>& a) { F0R(i,sz(a)) re(a[i]); }
     template<class T, size_t SZ> void re(array<T,SZ>& a) { F0R(i,SZ) re(a[i]); }
-    
-    // OUTPUT 
-    
-    template<class T1, class T2> ostream& operator<<(ostream& os, const pair<T1,T2>& a) {
-        os << '{' << a.f << ", " << a.s << '}'; return os;
-    }
-    template<class T> ostream& printArray(ostream& os, const T& a, int SZ) {
-        os << '{';
-        F0R(i,SZ) {
-            if (i) {
-                os << ", ";
-                if (is_like_array(a[i])) cout << "\n";
-            }
-            os << a[i];
-        }
-        os << '}';
-        return os;
-    }
-    template<class T, size_t SZ> ostream& operator<<(ostream& os, const array<T,SZ>& a) {
-        return printArray(os,a,SZ);
-    }
-    template<class T> ostream& operator<<(ostream& os, const vector<T>& a) {
-        return printArray(os,a,sz(a));
-    }
-    template<class T> ostream& operator<<(ostream& os, const set<T>& a) {
-        os << vector<T>(all(a)); return os;
-    }
-    template<class T1, class T2> ostream& operator<<(ostream& os, const map<T1,T2>& a) {
-        os << vector<pair<T1,T2>>(all(a)); return os;
-    }
-    
-    template<class T> void pr(const T& x) { cout << x << '\n'; }
+}
+
+using namespace input;
+
+namespace output {
+    template<class T1, class T2> void pr(const pair<T1,T2>& x);
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x);
+    template<class T> void pr(const vector<T>& x);
+    template<class T> void pr(const set<T>& x);
+    template<class T1, class T2> void pr(const map<T1,T2>& x);
+
+    template<class T> void pr(const T& x) { cout << x; }
     template<class Arg, class... Args> void pr(const Arg& first, const Args&... rest) { 
-        cout << first << ' '; pr(rest...); 
+        pr(first); pr(rest...); 
+    }
+
+    template<class T1, class T2> void pr(const pair<T1,T2>& x) { 
+        pr("{",x.f,", ",x.s,"}"); 
+    }
+    template<class T> void prContain(const T& x) {
+        pr("{");
+        bool fst = 1; trav(a,x) pr(!fst?", ":"",a), fst = 0; 
+        pr("}");
+    }
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x) { prContain(x); }
+    template<class T> void pr(const vector<T>& x) { prContain(x); }
+    template<class T> void pr(const set<T>& x) { prContain(x); }
+    template<class T1, class T2> void pr(const map<T1,T2>& x) { prContain(x); }
+    
+    template<class Arg> void ps(const Arg& first) { pr(first,"\n"); } // print w/ spaces
+    template<class Arg, class... Args> void ps(const Arg& first, const Args&... rest) { 
+        pr(first," "); ps(rest...); 
+    }
+}
+
+using namespace output;
+
+namespace io {
+    void setIn(string s) { freopen(s.c_str(),"r",stdin); }
+    void setOut(string s) { freopen(s.c_str(),"w",stdout); }
+    void setIO(string s = "") {
+        ios_base::sync_with_stdio(0); cin.tie(0); // fast I/O
+        if (sz(s)) { setIn(s+".in"), setOut(s+".out"); } // for USACO
     }
 }
 

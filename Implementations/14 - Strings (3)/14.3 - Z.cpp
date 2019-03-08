9eb71beac97620e3353740b21db15c6c0805181e
Benjamin Qi <bqi343@gmail.com>
2019-03-08 12:02:19 -0500

    edge case

--- Implementations/14 - Strings (3)/14.3 - Z.cpp
+++ Implementations/14 - Strings (3)/14.3 - Z.cpp
@@ -1,14 +1,13 @@
 /**
-* Source: http://codeforces.com/blog/entry/3107
-* Description: similar to KMP
-* Verification: POI 12 Template
-*/
+ * Source: http://codeforces.com/blog/entry/3107
+ * Description: similar to KMP
+ * Verification: POI 12 Template, https://codeforces.com/contest/1137/problem/B
+ */
 
 vi z(string s) {
     int N = sz(s); s += '#';
-    vi ans(N); ans[0] = N;
+    vi ans(N); ans[0] = N; if (N == 1) return ans;
     while (s[1+ans[1]] == s[ans[1]]) ans[1] ++;
-    
     int L = 1, R = ans[1];
     FOR(i,2,N) {
         if (i <= R) ans[i] = min(R-i+1,ans[i-L]);

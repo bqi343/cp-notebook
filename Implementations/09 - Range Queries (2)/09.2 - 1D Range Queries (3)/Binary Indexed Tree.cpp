c7506905666107afb1d0e7abd9de401657454b6d
Benjamin Qi <bqi343@gmail.com>
2019-03-18 10:10:27 -0400

    Update Binary Indexed Tree.cpp

--- Implementations/09 - Range Queries (2)/09.2 - 1D Range Queries (3)/Binary Indexed Tree.cpp
+++ Implementations/09 - Range Queries (2)/09.2 - 1D Range Queries (3)/Binary Indexed Tree.cpp
@@ -1,6 +1,8 @@
 /**
  * Description: N-D range sum query with point update
- * Source: https://codeforces.com/blog/entry/64914
+ * Sources: 
+    * https://codeforces.com/blog/entry/64914
+    * https://www.geeksforgeeks.org/binary-indexed-tree-range-update-range-queries/
  * Verification: SPOJ matsum
  */
 
@@ -26,11 +28,12 @@ template <class T, int N, int... Ns> struct BIT<T, N, Ns...> {
 
 template<class T, int SZ> struct BITrange {
     BIT<T,SZ> bit[2]; // piecewise linear functions
-    void upd(int hi, T val) {
-        bit[1].upd(1,val), bit[1].upd(hi+1,-val);
-        bit[0].upd(hi+1,hi*val);
+    // let cum[x] = sum_{i=1}^{x}a[i]
+    void upd(int hi, T val) { // add val to a[1..hi]
+        bit[1].upd(1,val), bit[1].upd(hi+1,-val); // if x <= hi, cum[x] += val*x
+        bit[0].upd(hi+1,hi*val); // if x > hi, cum[x] += val*hi
     }
     void upd(int lo, int hi, T val) { upd(lo-1,-val), upd(hi,val); }
-    T sum(int x) { return bit[1].sum(x)*x+bit[0].sum(x); }
+    T sum(int x) { return bit[1].sum(x)*x+bit[0].sum(x); } // get cum[x]
     T query(int x, int y) { return sum(y)-sum(x-1); }
 }; // equivalent to 1D lazy segment tree for sum

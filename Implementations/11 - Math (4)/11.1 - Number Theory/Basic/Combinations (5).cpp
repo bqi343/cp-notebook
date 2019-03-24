fc6eb1d5f6ad715c07f4a94170a37f93fbc7859a
Benjamin Qi <bqi343@gmail.com>
2019-03-24 10:26:07 -0400

    Update Combinations (5).cpp

--- Implementations/11 - Math (4)/11.1 - Number Theory/Basic/Combinations (5).cpp
+++ Implementations/11 - Math (4)/11.1 - Number Theory/Basic/Combinations (5).cpp
@@ -8,22 +8,39 @@
 using namespace basicFactor;
 
 template<int SZ> struct Combo {
-    int MOD; vpi factors;
-    mi fac[SZ], ifac[SZ]; vi cnt[SZ];
+    int MOD; 
+    mi smallInv[SZ], fac[SZ], ifac[SZ]; 
+    vpi factors; vi cnt[SZ];
 
+    void genInv() {
+        if (sz(factors) == 1 && factors[0].s == 1) {
+            F0R(i,2) smallInv[i] = mi(i,MOD);
+            FOR(i,2,SZ) { // O(SZ) operations
+                int x = (MOD+i-1)/i;
+                smallInv[i] = smallInv[i*x-MOD]*x;
+            }
+        } else {
+            F0R(i,SZ) {
+                int x = invGeneral(i,MOD);
+                if (x == -1) smallInv[i] = mi(0,MOD);
+                else smallInv[i] = mi(x,MOD);
+            }
+        }
+    }
+    
     void init(int _MOD) {
-        MOD = _MOD;
-        factors = factor(MOD);
-        // factors = {{MOD,1}};
+        MOD = _MOD; factors = factor(MOD);
+        factors = {{MOD,1}}; 
+        genInv();
+        
         cnt[0] = vi(sz(factors));
         fac[0] = ifac[0] = mi(1,MOD);
-
         FOR(i,1,SZ) {
             cnt[i] = cnt[i-1];
             int I = i;
             F0R(j,sz(factors)) while (I % factors[j].f == 0)
                 I /= factors[j].f, cnt[i][j] ++;
-            fac[i] = fac[i-1]*I; ifac[i] = 1/fac[i];
+            fac[i] = fac[i-1]*I; ifac[i] = ifac[i-1]*smallInv[I];
         }
     }
 

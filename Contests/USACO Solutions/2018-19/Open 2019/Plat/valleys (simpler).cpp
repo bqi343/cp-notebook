b7860ce99c205a6baff05f9b8bb2d388e721988e
Benjamin Qi <bqi343@gmail.com>
2019-04-12 12:03:06 -0400

    Create valleys (simpler).cpp

---
+++ Contests/USACO Solutions/2018-19/Open 2019/Plat/valleys (simpler).cpp
@@ -0,0 +1,267 @@
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/rope>
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= (a); i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a, x) for (auto& a : x)
+
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+#define sz(x) (int)x.size()
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) beg(x), en(x)
+#define resz resize
+
+const int MOD = 1000000007; // 998244353
+const ll INF = 1e18;
+const int MX = 100001;
+const ld PI = 4*atan((ld)1);
+
+template<class T> void ckmin(T &a, T b) { a = min(a, b); }
+template<class T> void ckmax(T &a, T b) { a = max(a, b); }
+
+template<class A, class B> A operator+=(A& l, const B& r) { return l = l+r; }
+template<class A, class B> A operator-=(A& l, const B& r) { return l = l-r; }
+template<class A, class B> A operator*=(A& l, const B& r) { return l = l*r; }
+template<class A, class B> A operator/=(A& l, const B& r) { return l = l/r; }
+
+namespace input {
+    template<class T> void re(complex<T>& x);
+    template<class T1, class T2> void re(pair<T1,T2>& p);
+    template<class T> void re(vector<T>& a);
+    template<class T, size_t SZ> void re(array<T,SZ>& a);
+
+    template<class T> void re(T& x) { cin >> x; }
+    void re(double& x) { string t; re(t); x = stod(t); }
+    void re(ld& x) { string t; re(t); x = stold(t); }
+    template<class Arg, class... Args> void re(Arg& first, Args&... rest) { 
+        re(first); re(rest...); 
+    }
+
+    template<class T> void re(complex<T>& x) { T a,b; re(a,b); x = cd(a,b); }
+    template<class T1, class T2> void re(pair<T1,T2>& p) { re(p.f,p.s); }
+    template<class T> void re(vector<T>& a) { F0R(i,sz(a)) re(a[i]); }
+    template<class T, size_t SZ> void re(array<T,SZ>& a) { F0R(i,SZ) re(a[i]); }
+}
+
+using namespace input;
+
+namespace output {
+    template<class T1, class T2> void pr(const pair<T1,T2>& x);
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x);
+    template<class T> void pr(const vector<T>& x);
+    template<class T> void pr(const set<T>& x);
+    template<class T1, class T2> void pr(const map<T1,T2>& x);
+
+    template<class T> void pr(const T& x) { cout << x; }
+    template<class Arg, class... Args> void pr(const Arg& first, const Args&... rest) { 
+        pr(first); pr(rest...); 
+    }
+
+    template<class T1, class T2> void pr(const pair<T1,T2>& x) { 
+        pr("{",x.f,", ",x.s,"}"); 
+    }
+    template<class T> void prContain(const T& x) {
+        pr("{");
+        bool fst = 1; trav(a,x) pr(!fst?", ":"",a), fst = 0; 
+        pr("}");
+    }
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x) { prContain(x); }
+    template<class T> void pr(const vector<T>& x) { prContain(x); }
+    template<class T> void pr(const set<T>& x) { prContain(x); }
+    template<class T1, class T2> void pr(const map<T1,T2>& x) { prContain(x); }
+    
+    void ps() { pr("\n"); } 
+    template<class Arg, class... Args> void ps(const Arg& first, const Args&... rest) { 
+        pr(first," "); ps(rest...); // print w/ spaces
+    }
+}
+
+using namespace output;
+
+namespace io {
+    void setIn(string s) { freopen(s.c_str(),"r",stdin); }
+    void setOut(string s) { freopen(s.c_str(),"w",stdout); }
+    void setIO(string s = "") {
+        ios_base::sync_with_stdio(0); cin.tie(0); // fast I/O
+        if (sz(s)) { setIn(s+".in"), setOut(s+".out"); } // for USACO
+    }
+}
+
+using namespace io;
+
+template<class T> T invGeneral(T a, T b) {
+    a %= b; if (a == 0) return b == 1 ? 0 : -1;
+    T x = invGeneral(b,a); 
+    return x == -1 ? -1 : ((1-(ll)b*x)/a+b)%b;
+}
+
+template<class T> struct modInt {
+    T val;
+    T mod = 0;
+    // static const T mod = MOD;
+
+    void normalize() {
+        if (mod == 0) return;
+        val %= mod; if (val < 0) val += mod;
+    }
+    modInt(T v = 0, T m = 0) : val(v), mod(m) { normalize(); }
+    // modInt(T v = 0, T m = 0) : val(v) { normalize(); }
+
+    explicit operator T() const { return val; }
+    friend ostream& operator<<(ostream& os, const modInt& a) { return os << a.val; }
+    friend bool operator==(const modInt& a, const modInt& b) { return a.val == b.val; }
+    friend bool operator!=(const modInt& a, const modInt& b) { return !(a == b); }
+
+    friend void check(modInt& a, modInt& b) { // make sure all operations are valid
+        // comment out if mod is static const
+        if (a.mod > 0 && b.mod > 0) { assert(a.mod == b.mod); return; }
+        T mod = max(a.mod,b.mod); if (mod == 0) mod = MOD;
+        if (a.mod != mod) { a.mod = mod; a.normalize(); }
+        if (b.mod != mod) { b.mod = mod; b.normalize(); }
+    }
+    friend modInt operator+(modInt a, modInt b) {
+        check(a,b); a.val += (T)b;
+        if (a.val >= a.mod) a.val -= a.mod;
+        return a;
+    }
+    friend modInt operator-(modInt a, modInt b) {
+        check(a,b); a.val -= (T)b; 
+        if (a.val < 0) a.val += a.mod; 
+        return a;
+    }
+    friend modInt operator-(const modInt& a) { return modInt(0)-a; }
+
+    friend modInt operator*(modInt a, modInt b) {
+        check(a,b); a.val = (ll)a.val*(T)b%a.mod; return a;
+    }
+    friend modInt exp(modInt a, ll p) {
+        modInt ans(1,a.mod);
+        for (; p; p /= 2, a *= a) if (p&1) ans *= a;
+        return ans;
+    }
+    friend modInt inv(const modInt& a) {
+        return {invGeneral(a.val,a.mod),a.mod};
+        // return exp(b,b.mod-2) if prime
+    }
+    friend modInt operator/(modInt a, modInt b) { 
+        check(a,b); return a*inv(b); 
+    }
+};
+
+typedef modInt<int> mi;
+typedef pair<mi,mi> pmi;
+typedef vector<mi> vmi;
+typedef vector<pmi> vpmi;
+
+int N, h[750][750];
+
+int hsh(pi a) {
+    return N*a.f+a.s;
+}
+
+bool valid(pi a) {
+    return 0 <= a.f && a.f < N && 0 <= a.s && a.s < N;
+}
+
+template<int SZ> struct DSU {
+    int par[SZ], sz[SZ], hole[SZ], numComp = 1;
+
+    DSU() {
+        F0R(i,SZ) par[i] = i, sz[i] = 1;
+    }
+
+    int get(int x) { // path compression
+        if (par[x] != x) par[x] = get(par[x]);
+        return par[x];
+    }
+
+    void unite(pi a, pi b) { // union-by-rank
+        int A = get(hsh(a)), B = get(hsh(b));
+        if (A == B) return;
+        if (sz[A] < sz[B]) swap(A,B);
+        numComp --;
+        sz[A] += sz[B]; hole[A] += hole[B]; par[B] = A;
+    }
+};
+
+DSU<750*750+5> A,B;
+
+int xd[4] = {0,1,0,-1}, yd[4] = {1,0,-1,0};
+
+int main() {
+    setIO("valleys"); re(N);
+    F0R(i,N) F0R(j,N) re(h[i][j]);
+    vpi v;
+    F0R(i,N) F0R(j,N) v.pb({i,j});
+    sort(all(v), [](pi a, pi b) { return h[a.f][a.s] < h[b.f][b.s]; });
+    
+    vi rec = {1};
+    F0Rd(i,sz(v)) {
+        A.numComp ++;
+        FOR(j,-1,2) FOR(k,-1,2) {
+            pi x = {v[i].f+j,v[i].s+k};
+            if (!valid(x)) A.unite(v[i],{N,0});
+            else if (h[x.f][x.s] > h[v[i].f][v[i].s]) A.unite(v[i],x);
+        }
+        rec.pb(A.numComp);
+    }
+    reverse(all(rec));
+    
+    ll ret = 0;
+    F0R(i,sz(v)) { // maintain number of holes in current component
+        F0R(j,4) {
+            pi x = {v[i].f+xd[j],v[i].s+yd[j]};
+            if (!valid(x)) continue;
+            if (h[x.f][x.s] < h[v[i].f][v[i].s]) B.unite(v[i],x);
+        }
+        int H = B.get(hsh(v[i]));
+        B.hole[H] += rec[i+1]-rec[i];
+        if (B.hole[H] == 0) {
+            ret += B.sz[H];
+            // ps("??",B.sz[H]);
+        }
+        // rec[0] -> rec[1]
+    }
+    pr(ret);
+}
+
+/* stuff you should look for
+    * int overflow, array bounds
+    * special cases (n=1?), set tle
+    * do smth instead of nothing and stay organized
+*/

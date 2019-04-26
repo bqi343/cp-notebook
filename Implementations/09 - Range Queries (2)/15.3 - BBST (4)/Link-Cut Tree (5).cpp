76c2f576a8c9479f536ab397be748f0ced6d2ac7
Benq <bqi343@gmail.com>
2019-04-26 18:15:29 -0400

    splay tree typedef

--- Implementations/09 - Range Queries (2)/15.3 - BBST (4)/Link-Cut Tree (5).cpp
+++ Implementations/09 - Range Queries (2)/15.3 - BBST (4)/Link-Cut Tree (5).cpp
@@ -8,36 +8,36 @@
 using namespace splayTree;
 
 template<int SZ> struct LCT {
-    ps S[SZ];
+    sn S[SZ];
     LCT () { F0R(i,SZ) S[i] = new snode(i); }
 
     // disconnect x from d-th child
-    void dis(ps x, int d) { 
-        ps y = x->c[d]; 
+    void dis(sn x, int d) { 
+        sn y = x->c[d]; 
         if (x) x->c[d] = NULL, recalc(x);
         if (y) { y->p = NULL; if (d) y->pp = x; }
     }
     // set x to be child of pp
-    void makeChild(ps x) { setLink(x->pp,x,1); x->pp = NULL; } 
+    void makeChild(sn x) { setLink(x->pp,x,1); x->pp = NULL; } 
     // unlink x->pp from its preferred child, then set x to be preferred child 
-    void setPref(ps x) { splay(x->pp), dis(x->pp,1), makeChild(x), splay(x); } 
+    void setPref(sn x) { splay(x->pp), dis(x->pp,1), makeChild(x), splay(x); } 
     // x is brought to the root of auxiliary tree
-    ps access(ps x) { dis(splay(x),1); while (x->pp) setPref(x); return x; }
+    sn access(sn x) { dis(splay(x),1); while (x->pp) setPref(x); return x; }
     
     //////// UPDATES
     
-    ps makeRoot(ps v) { access(v)->flip = 1; return access(v); }
+    sn makeRoot(sn v) { access(v)->flip = 1; return access(v); }
     // make y the parent of x
-    void link(ps x, ps y) { makeRoot(x)->pp = y; }
+    void link(sn x, sn y) { makeRoot(x)->pp = y; }
     // cut link between x and its parent
-    void cut(ps x) { dis(access(x),0); }
+    void cut(sn x) { dis(access(x),0); }
     
     //////// QUERIES
     
-    int getDepth(ps x) { access(x); return getsz(x->c[0]); }
-    ps getRoot(ps x) { return farthest(access(x),0); }
-    ps lca(ps x, ps y) {
-        ps root = getRoot(y); 
+    int getDepth(sn x) { access(x); return getsz(x->c[0]); }
+    sn getRoot(sn x) { return farthest(access(x),0); }
+    sn lca(sn x, sn y) {
+        sn root = getRoot(y); 
         if (farthest(splay(x),0) == root) return x;
         while (splay(x)->pp) {
             if (farthest(splay(x->pp),0) == root) return x->pp;
--- Implementations/09 - Range Queries (2)/15.3 - BBST (4)/Splay Tree (5).cpp
+++ Implementations/09 - Range Queries (2)/15.3 - BBST (4)/Splay Tree (5).cpp
@@ -5,11 +5,11 @@
  */
 
 namespace splayTree {
-    typedef struct snode* ps;
+    typedef struct snode* sn;
 
     struct snode {
         int val, sz; // value, # nodes in subtree
-        ps p, pp, c[2]; // parent, path-parent (for LCT), children
+        sn p, pp, c[2]; // parent, path-parent (for LCT), children
         bool flip; // for range flip 
 
         snode (int val) : val(val) { 
@@ -19,44 +19,44 @@ namespace splayTree {
         }
     };
 
-    int getsz(ps x) { return x ? x->sz : 0; }
-    int dir(ps x, ps y) { return x ? (x->c[1] == y) : -1; }
-    ps recalc(ps x) {
+    int getsz(sn x) { return x ? x->sz : 0; }
+    int dir(sn x, sn y) { return x ? (x->c[1] == y) : -1; }
+    sn recalc(sn x) {
         x->sz = 1+getsz(x->c[0])+getsz(x->c[1]);
         return x;
     }
-    void prop(ps x) {
+    void prop(sn x) {
         if (!x || !x->flip) return; 
         swap(x->c[0],x->c[1]);
         if (x->c[0]) x->c[0]->flip ^= 1;
         if (x->c[1]) x->c[1]->flip ^= 1;
         x->flip = 0;
     }
-    void propAnc(ps x) { // propagate all ancestors
+    void propAnc(sn x) { // propagate all ancestors
         if (!x) return;
         if (x->p) propAnc(x->p);
         prop(x);
     }
-    void tour(ps x, vi& v) {
+    void tour(sn x, vi& v) {
         if (!x) return;
         tour(x->c[0],v); v.pb(x->val); tour(x->c[1],v);
     }
 
-    void setLink(ps x, ps y, int d) { // x propagated
+    void setLink(sn x, sn y, int d) { // x propagated
         if (x) x->c[d] = y, recalc(x);
         if (y) y->p = x;
     }
-    void rot(ps x, int d) { // precondition: x & parents propagated
-        ps y = x->c[d], z = x->p; prop(y); 
+    void rot(sn x, int d) { // precondition: x & parents propagated
+        sn y = x->c[d], z = x->p; prop(y); 
         setLink(x, y->c[d^1], d);
         setLink(y, x, d^1);
         setLink(z, y, dir(z, x));
         y->pp = x->pp; x->pp = NULL; // set y to be parent of x
     }
-    ps splay(ps x) {
+    sn splay(sn x) {
         propAnc(x);
         while (x && x->p) {
-            ps y = x->p, z = y->p;
+            sn y = x->p, z = y->p;
             int dy = dir(y, x), dz = dir(z, y);
             if (!z) rot(y, dy);
             else if (dy == dz) rot(z, dz), rot(y, dy);
@@ -64,7 +64,7 @@ namespace splayTree {
         }
         return x;
     }
-    ps farthest(ps x, int d) { // get leftmost or rightmost node
+    sn farthest(sn x, int d) { // get leftmost or rightmost node
         prop(x); return x->c[d]?farthest(x->c[d],d):splay(x);
     }
 }

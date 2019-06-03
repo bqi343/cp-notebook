e63089b949349be2c40417789228f8bf610790c4
Benjamin Qi <bqi343@gmail.com>
2019-06-02 20:21:43 -0400

    Gold

---
+++ Contests/USACO Solutions/2018-19/Open 2019/Gold/balance.cpp
@@ -0,0 +1,230 @@
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/rope>
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= (a); i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a, x) for (auto& a : x)
+
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+#define sz(x) (int)x.size()
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) beg(x), en(x)
+#define resz resize
+
+const int MOD = 1000000007;
+const ll INF = 1e18;
+const int MX = 200005;
+const ld PI = 4*atan((ld)1);
+
+template<class T> void ckmin(T &a, T b) { a = min(a, b); }
+template<class T> void ckmax(T &a, T b) { a = max(a, b); }
+
+namespace input {
+    template<class T> void re(complex<T>& x);
+    template<class T1, class T2> void re(pair<T1,T2>& p);
+    template<class T> void re(vector<T>& a);
+    template<class T, size_t SZ> void re(array<T,SZ>& a);
+
+    template<class T> void re(T& x) { cin >> x; }
+    void re(double& x) { string t; re(t); x = stod(t); }
+    void re(ld& x) { string t; re(t); x = stold(t); }
+    template<class Arg, class... Args> void re(Arg& first, Args&... rest) { 
+        re(first); re(rest...); 
+    }
+
+    template<class T> void re(complex<T>& x) { T a,b; re(a,b); x = cd(a,b); }
+    template<class T1, class T2> void re(pair<T1,T2>& p) { re(p.f,p.s); }
+    template<class T> void re(vector<T>& a) { F0R(i,sz(a)) re(a[i]); }
+    template<class T, size_t SZ> void re(array<T,SZ>& a) { F0R(i,SZ) re(a[i]); }
+}
+
+using namespace input;
+
+namespace output {
+    template<class T1, class T2> void pr(const pair<T1,T2>& x);
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x);
+    template<class T> void pr(const vector<T>& x);
+    template<class T> void pr(const set<T>& x);
+    template<class T1, class T2> void pr(const map<T1,T2>& x);
+
+    template<class T> void pr(const T& x) { cout << x; }
+    template<class Arg, class... Args> void pr(const Arg& first, const Args&... rest) { 
+        pr(first); pr(rest...); 
+    }
+
+    template<class T1, class T2> void pr(const pair<T1,T2>& x) { 
+        pr("{",x.f,", ",x.s,"}"); 
+    }
+    template<class T> void prContain(const T& x) {
+        pr("{");
+        bool fst = 1; for (const auto& a: x) pr(!fst?", ":"",a), fst = 0; // const needed for vector<bool>
+        pr("}");
+    }
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x) { prContain(x); }
+    template<class T> void pr(const vector<T>& x) { prContain(x); }
+    template<class T> void pr(const set<T>& x) { prContain(x); }
+    template<class T1, class T2> void pr(const map<T1,T2>& x) { prContain(x); }
+    
+    void ps() { pr("\n"); }
+    template<class Arg> void ps(const Arg& first) { 
+        pr(first); ps(); // no space at end of line
+    }
+    template<class Arg, class... Args> void ps(const Arg& first, const Args&... rest) { 
+        pr(first," "); ps(rest...); // print w/ spaces
+    }
+}
+
+using namespace output;
+
+namespace io {
+    void setIn(string s) { freopen(s.c_str(),"r",stdin); }
+    void setOut(string s) { freopen(s.c_str(),"w",stdout); }
+    void setIO(string s = "") {
+        ios_base::sync_with_stdio(0); cin.tie(0); // fast I/O
+        if (sz(s)) { setIn(s+".in"), setOut(s+".out"); } // for USACO
+    }
+}
+
+using namespace io;
+
+template<class T> T invGeneral(T a, T b) {
+    a %= b; if (a == 0) return b == 1 ? 0 : -1;
+    T x = invGeneral(b,a); 
+    return x == -1 ? -1 : ((1-(ll)b*x)/a+b)%b;
+}
+
+template<class T> struct modular {
+    T val; 
+    explicit operator T() const { return val; }
+    modular() { val = 0; }
+    template<class U> modular(const U& v) {
+        val = (-MOD <= v && v <= MOD) ? v : v % MOD;
+        if (val < 0) val += MOD;
+    }
+    friend ostream& operator<<(ostream& os, const modular& a) { return os << a.val; }
+    friend bool operator==(const modular& a, const modular& b) { return a.val == b.val; }
+    friend bool operator!=(const modular& a, const modular& b) { return !(a == b); }
+
+    modular operator-() const { return modular(-val); }
+    modular& operator+=(const modular& m) { if ((val += m.val) >= MOD) val -= MOD; return *this; }
+    modular& operator-=(const modular& m) { if ((val -= m.val) < 0) val += MOD; return *this; }
+    modular& operator*=(const modular& m) { val = (ll)val*m.val%MOD; return *this; }
+    friend modular exp(modular a, ll p) {
+        modular ans = 1; for (; p; p /= 2, a *= a) if (p&1) ans *= a;
+        return ans;
+    }
+    friend modular inv(const modular& a) { return invGeneral(a.val,MOD); } 
+    // inv is equivalent to return exp(b,b.mod-2) if prime
+    modular& operator/=(const modular& m) { return (*this) *= inv(m); }
+    
+    friend modular operator+(modular a, const modular& b) { return a += b; }
+    friend modular operator-(modular a, const modular& b) { return a -= b; }
+    friend modular operator*(modular a, const modular& b) { return a *= b; }
+    
+    friend modular operator/(modular a, const modular& b) { return a /= b; }
+};
+
+typedef modular<int> mi;
+typedef pair<mi,mi> pmi;
+typedef vector<mi> vmi;
+typedef vector<pmi> vpmi;
+
+int N;
+vi v0, v1;
+ll dif, ans = INF, change;
+
+ll countInv(vi v) {
+    ll res = 0, co = 0;
+    F0R(i,sz(v)) {
+        if (v[i]) co ++;
+        else res += co;
+    }
+    return res;
+}
+
+int numOnes(vi v) {
+    int sum = 0;
+    trav(t,v) sum += t;
+    return sum;
+}
+
+int main() {
+    setIO("balance"); 
+    re(N); v0.resz(N), v1.resz(N); re(v0,v1);
+    change -= N; change += numOnes(v0)+numOnes(v1);
+    dif = countInv(v0)-countInv(v1);
+    
+    vi pos0, pos1;
+    F0Rd(i,N) if (v0[i] == 1) pos0.pb(i);
+    F0R(i,N) if (v1[i] == 0) pos1.pb(i);
+    ll moves0 = 0, moves1 = 0, cdif = dif;
+    // ps("??",dif,change);
+    int mx = min(sz(pos0),sz(pos1));
+    F0R(i,mx+1) {
+        ckmin(ans,moves0+moves1+abs(cdif-moves0+moves1)+(ll)i*i);
+        if (i != mx) {
+            moves0 += N-1-i-pos0[i];
+            moves1 += pos1[i]-i;
+            cdif += change; // ps("??",change,cdif,ans,need);
+        }
+    }
+    
+    pos0.clear(), pos1.clear();
+    F0Rd(i,N) if (v0[i] == 0) pos0.pb(i);
+    F0R(i,N) if (v1[i] == 1) pos1.pb(i);
+    moves0 = moves1 = 0, cdif = dif;
+    mx = min(sz(pos0),sz(pos1));
+    F0R(i,mx+1) {
+        ckmin(ans,moves0+moves1+abs(cdif+moves0-moves1)+(ll)i*i);
+        if (i != mx) {
+            moves0 += N-1-i-pos0[i];
+            moves1 += pos1[i]-i;
+            cdif -= change; // ps("??",change,cdif,ans,need);
+        }
+    }
+    
+    // 1,0 -> 0,1 makes dif += #ones-n
+    ps(ans);
+    // you should actually read the stuff at the bottom
+}
+
+/* stuff you should look for
+    * int overflow, array bounds
+    * special cases (n=1?), set tle
+    * do smth instead of nothing and stay organized
+*/
---
+++ Contests/USACO Solutions/2018-19/Open 2019/Gold/walk.cpp
@@ -0,0 +1,195 @@
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/rope>
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= (a); i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a, x) for (auto& a : x)
+
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+#define sz(x) (int)x.size()
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) beg(x), en(x)
+#define resz resize
+
+const int MOD = 1000000007;
+const ll INF = 1e18;
+const int MX = 200005;
+const ld PI = 4*atan((ld)1);
+
+template<class T> void ckmin(T &a, T b) { a = min(a, b); }
+template<class T> void ckmax(T &a, T b) { a = max(a, b); }
+
+namespace input {
+    template<class T> void re(complex<T>& x);
+    template<class T1, class T2> void re(pair<T1,T2>& p);
+    template<class T> void re(vector<T>& a);
+    template<class T, size_t SZ> void re(array<T,SZ>& a);
+
+    template<class T> void re(T& x) { cin >> x; }
+    void re(double& x) { string t; re(t); x = stod(t); }
+    void re(ld& x) { string t; re(t); x = stold(t); }
+    template<class Arg, class... Args> void re(Arg& first, Args&... rest) { 
+        re(first); re(rest...); 
+    }
+
+    template<class T> void re(complex<T>& x) { T a,b; re(a,b); x = cd(a,b); }
+    template<class T1, class T2> void re(pair<T1,T2>& p) { re(p.f,p.s); }
+    template<class T> void re(vector<T>& a) { F0R(i,sz(a)) re(a[i]); }
+    template<class T, size_t SZ> void re(array<T,SZ>& a) { F0R(i,SZ) re(a[i]); }
+}
+
+using namespace input;
+
+namespace output {
+    template<class T1, class T2> void pr(const pair<T1,T2>& x);
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x);
+    template<class T> void pr(const vector<T>& x);
+    template<class T> void pr(const set<T>& x);
+    template<class T1, class T2> void pr(const map<T1,T2>& x);
+
+    template<class T> void pr(const T& x) { cout << x; }
+    template<class Arg, class... Args> void pr(const Arg& first, const Args&... rest) { 
+        pr(first); pr(rest...); 
+    }
+
+    template<class T1, class T2> void pr(const pair<T1,T2>& x) { 
+        pr("{",x.f,", ",x.s,"}"); 
+    }
+    template<class T> void prContain(const T& x) {
+        pr("{");
+        bool fst = 1; for (const auto& a: x) pr(!fst?", ":"",a), fst = 0; // const needed for vector<bool>
+        pr("}");
+    }
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x) { prContain(x); }
+    template<class T> void pr(const vector<T>& x) { prContain(x); }
+    template<class T> void pr(const set<T>& x) { prContain(x); }
+    template<class T1, class T2> void pr(const map<T1,T2>& x) { prContain(x); }
+    
+    void ps() { pr("\n"); }
+    template<class Arg> void ps(const Arg& first) { 
+        pr(first); ps(); // no space at end of line
+    }
+    template<class Arg, class... Args> void ps(const Arg& first, const Args&... rest) { 
+        pr(first," "); ps(rest...); // print w/ spaces
+    }
+}
+
+using namespace output;
+
+namespace io {
+    void setIn(string s) { freopen(s.c_str(),"r",stdin); }
+    void setOut(string s) { freopen(s.c_str(),"w",stdout); }
+    void setIO(string s = "") {
+        ios_base::sync_with_stdio(0); cin.tie(0); // fast I/O
+        if (sz(s)) { setIn(s+".in"), setOut(s+".out"); } // for USACO
+    }
+}
+
+using namespace io;
+
+template<class T> T invGeneral(T a, T b) {
+    a %= b; if (a == 0) return b == 1 ? 0 : -1;
+    T x = invGeneral(b,a); 
+    return x == -1 ? -1 : ((1-(ll)b*x)/a+b)%b;
+}
+
+template<class T> struct modular {
+    T val; 
+    explicit operator T() const { return val; }
+    modular() { val = 0; }
+    template<class U> modular(const U& v) {
+        val = (-MOD <= v && v <= MOD) ? v : v % MOD;
+        if (val < 0) val += MOD;
+    }
+    friend ostream& operator<<(ostream& os, const modular& a) { return os << a.val; }
+    friend bool operator==(const modular& a, const modular& b) { return a.val == b.val; }
+    friend bool operator!=(const modular& a, const modular& b) { return !(a == b); }
+
+    modular operator-() const { return modular(-val); }
+    modular& operator+=(const modular& m) { if ((val += m.val) >= MOD) val -= MOD; return *this; }
+    modular& operator-=(const modular& m) { if ((val -= m.val) < 0) val += MOD; return *this; }
+    modular& operator*=(const modular& m) { val = (ll)val*m.val%MOD; return *this; }
+    friend modular exp(modular a, ll p) {
+        modular ans = 1; for (; p; p /= 2, a *= a) if (p&1) ans *= a;
+        return ans;
+    }
+    friend modular inv(const modular& a) { return invGeneral(a.val,MOD); } 
+    // inv is equivalent to return exp(b,b.mod-2) if prime
+    modular& operator/=(const modular& m) { return (*this) *= inv(m); }
+    
+    friend modular operator+(modular a, const modular& b) { return a += b; }
+    friend modular operator-(modular a, const modular& b) { return a -= b; }
+    friend modular operator*(modular a, const modular& b) { return a *= b; }
+    
+    friend modular operator/(modular a, const modular& b) { return a /= b; }
+};
+
+typedef modular<int> mi;
+typedef pair<mi,mi> pmi;
+typedef vector<mi> vmi;
+typedef vector<pmi> vpmi;
+
+int N,K,dis[7501];
+bitset<7501> vis;
+vi ed;
+
+int dist(int a, int b) {
+    return (2019201913LL*min(a,b)+2019201949LL*max(a,b)) % 2019201997LL;
+}
+
+int main() {
+    setIO("walk");
+    re(N,K); FOR(i,2,N+1) dis[i] = INT_MAX;
+    F0R(i,N) {
+        int bes = 0;
+        FOR(j,1,N+1) if (!vis[j] && (!bes || dis[j] < dis[bes])) bes = j;
+        if (dis[bes] > 0) ed.pb(dis[bes]);
+        vis[bes] = 1;
+        FOR(j,1,N+1) if (!vis[j]) ckmin(dis[j],dist(bes,j));
+    }
+    sort(all(ed));
+    // ps(ed);
+    ps(ed[N-K]);
+    // you should actually read the stuff at the bottom
+}
+
+/* stuff you should look for
+    * int overflow, array bounds
+    * special cases (n=1?), set tle
+    * do smth instead of nothing and stay organized
+*/

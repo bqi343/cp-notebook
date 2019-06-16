e9665d0c8bf13e24f82708dd486ebf3e191e3df2
Benjamin Qi <bqi343@gmail.com>
2019-06-16 16:08:40 -0400

    Update Linear Programming.cpp

--- Implementations/13 - Geometry (4)/Advanced (6)/Linear Programming.cpp
+++ Implementations/13 - Geometry (4)/Advanced (6)/Linear Programming.cpp
@@ -1,18 +1,22 @@
 /**
  * Description: Simplex Algorithm for linear programming
-    * maximize cˆT x subject to Ax <= b, x >= 0; (oops I should try to understand all the code)
+    * maximize cˆT x subject to Ax <= b, x >= 0
+	* https://www.utdallas.edu/~scniu/OPRE-6201/documents/LP06-Simplex-Tableau.pdf
+	* http://www.columbia.edu/~cs2035/courses/ieor3608.F05/bigm1.pdf
  * Usage: 
  	* https://open.kattis.com/contests/fvfhq4/problems/goatropes
  	* http://codeforces.com/contest/375/problem/E
+	* USACO Training cowwars
  * Source: KACTL, Stanford
+	* https://cs.stanford.edu/group/acm/SLPC/notebook.pdf
  */
  
 typedef double T;
 typedef vector<T> vd;
 typedef vector<vd> vvd;
-
 const T eps = 1e-8, inf = 1/.0;
-#define ltj(X) if(s == -1 || mp(X[j],N[j]) < mp(X[s],N[s])) s=j
+
+#define ltj(X) if (s == -1 || mp(X[j],N[j]) < mp(X[s],N[s])) s=j
 
 struct LPSolver {
 	int m, n;
@@ -22,51 +26,57 @@ struct LPSolver {
 	LPSolver(const vvd& A, const vd& b, const vd& c) :
 		m(sz(b)), n(sz(c)), N(n+1), B(m), D(m+2, vd(n+2)) {
 			F0R(i,m) F0R(j,n) D[i][j] = A[i][j];
-			F0R(i,m) { B[i] = n+i; D[i][n] = -1; D[i][n+1] = b[i];}
-			F0R(j,n) { N[j] = j; D[m][j] = -c[j]; }
+			F0R(i,m) { B[i] = n+i; D[i][n] = -1; D[i][n+1] = b[i]; } // B[i] -> basic variables, col n+1 is for constants, why D[i][n]=-1?
+			F0R(j,n) { N[j] = j; D[m][j] = -c[j]; } // N[j] -> non-basic variables, all zero
 			N[n] = -1; D[m+1][n] = 1;
 		}
 
+    void print() {
+        ps("D");
+        trav(t,D) ps(t);
+        ps();
+        ps("B",B); 
+        ps("N",N);
+        ps();
+    }
+    
 	void pivot(int r, int s) { // row, column
-		T *a = D[r].data(), inv = 1 / a[s]; // eliminate col s from consideration?
+		T *a = D[r].data(), inv = 1/a[s]; // eliminate col s from consideration
 		F0R(i,m+2) if (i != r && abs(D[i][s]) > eps) {
-			T *b = D[i].data(), inv2 = b[s] * inv;
-			F0R(j,n+2) b[j] -= a[j] * inv2;
-			b[s] = a[s] * inv2;
+			T *b = D[i].data(), inv2 = b[s]*inv;
+			F0R(j,n+2) b[j] -= a[j]*inv2;
+			b[s] = a[s]*inv2;
 		}
 		F0R(j,n+2) if (j != s) D[r][j] *= inv;
 		F0R(i,m+2) if (i != r) D[i][s] *= -inv;
-		D[r][s] = inv;
-		swap(B[r], N[s]);
+		D[r][s] = inv; swap(B[r], N[s]); // swap a basic and non-basic variable
 	}
 
 	bool simplex(int phase) {
-		int x = m + phase - 1;
+		int x = m+phase-1;
 		for (;;) {
-			int s = -1;
-			F0R(j,n+1) if (N[j] != -phase) ltj(D[x]); // find most negative col
-			if (D[x][s] >= -eps) return true;
+			int s = -1; F0R(j,n+1) if (N[j] != -phase) ltj(D[x]); // find most negative col
+			if (D[x][s] >= -eps) return true; // have best solution
 			int r = -1;
 		    F0R(i,m) {
 				if (D[i][s] <= eps) continue;
 				if (r == -1 || mp(D[i][n+1] / D[i][s], B[i])
 				             < mp(D[r][n+1] / D[r][s], B[r])) r = i; // find smallest positive ratio
 			}
-			if (r == -1) return false;
-			pivot(r, s); 
+			if (r == -1) return false; // unbounded
+			pivot(r, s);
 		}
 	}
 
 	T solve(vd &x) {
-		int r = 0;
-    		FOR(i,1,m) if (D[i][n+1] < D[r][n+1]) r = i;
-		if (D[r][n+1] < -eps) {
-			pivot(r, n);
-			if (!simplex(2) || D[m+1][n+1] < -eps) return -inf;
+		int r = 0; FOR(i,1,m) if (D[i][n+1] < D[r][n+1]) r = i;
+		if (D[r][n+1] < -eps) { // x=0 is not a solution
+			pivot(r, n); // -1 is artificial variable, initially set to smth large but want to get to 0 
+			if (!simplex(2) || D[m+1][n+1] < -eps) return -inf; // no solution
+			// D[m+1][n+1] is max possible value of the negation of artificial variable, starts negative but should get to zero
 			F0R(i,m) if (B[i] == -1) {
-				int s = 0;
-				FOR(j,1,n+1) ltj(D[i]);
-				pivot(i, s);
+				int s = 0; FOR(j,1,n+1) ltj(D[i]);
+				pivot(i,s);
 			}
 		}
 		bool ok = simplex(1); x = vd(n);
@@ -74,13 +84,3 @@ struct LPSolver {
 		return ok ? D[m][n+1] : inf;
 	}
 };
-
-/*vvd A; vd B,C;
-int co = 0; // # extra variables
-
-void addIneq(vd a, double b) { // a*x <= b
-    F0R(i,co) a.pb(0);
-    a.pb(1); co ++;
-    trav(z,A) z.pb(0);
-    A.pb(a); B.pb(b); C.pb(0);
-}*/

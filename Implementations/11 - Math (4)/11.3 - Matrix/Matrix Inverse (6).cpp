0c346d1767736df3f54f7452fedacece9238c6af
Benq <bqi343@gmail.com>
2019-06-18 22:25:45 -0400

    remove pointers from matrix

--- Implementations/11 - Math (4)/11.3 - Matrix/Matrix Inverse (6).cpp
+++ Implementations/11 - Math (4)/11.3 - Matrix/Matrix Inverse (6).cpp
@@ -7,21 +7,21 @@
 namespace matInv {
     template<class T> void elim(Mat<T>& m, int col, int a, int b) { // eliminate row a from row b
         auto x = m.d[b][col];
-        FOR(i,col,m.b) m.d[b][i] -= x*m.d[a][i];
+        FOR(i,col,m.c) m.d[b][i] -= x*m.d[a][i];
     }
     template<class T> T gauss(Mat<T>& m) { // determinant of 1000x1000 Matrix in ~1s
         T prod = 1; int nex = 0;
 
-        F0R(i,m.a) {
+        F0R(i,m.r) {
             int row = -1;
-            FOR(j,nex,m.a) if (m.d[j][i] != 0) { row = j; break; } // for ld use EPS
+            FOR(j,nex,m.r) if (m.d[j][i] != 0) { row = j; break; } // for ld use EPS
             if (row == -1) { prod = 0; continue; }
             if (row != nex) prod *= -1, swap(m.d[row],m.d[nex]);
 
             prod *= m.d[nex][i];
-            auto x = 1/m.d[nex][i]; FOR(k,i,m.b) m.d[nex][k] *= x;
+            auto x = 1/m.d[nex][i]; FOR(k,i,m.c) m.d[nex][k] *= x;
 
-            F0R(k,m.a) if (k != nex) elim(m,i,nex,k);
+            F0R(k,m.r) if (k != nex) elim(m,i,nex,k);
             nex ++;
         }
 
@@ -29,8 +29,8 @@ namespace matInv {
     }
 
     mi numSpan(Mat<mi> m) { // Kirchhoff's theorem
-        Mat<mi> res(m.a-1,m.a-1);
-        F0R(i,m.a) FOR(j,i+1,m.a) {
+        Mat<mi> res(m.r-1,m.r-1);
+        F0R(i,m.r) FOR(j,i+1,m.r) {
             if (i) {
                 res.d[i-1][i-1] += m.d[i][j];
                 res.d[i-1][j-1] -= m.d[i][j];
@@ -42,14 +42,14 @@ namespace matInv {
     }
 
     template<class T> Mat<T> inv(Mat<T> m) {
-        Mat<T> x(m.a,2*m.a);
-        F0R(i,m.a) F0R(j,m.a) x.d[i][j] = m.d[i][j];
-        F0R(i,m.a) x.d[i][i+m.a] = 1;
+        Mat<T> x(m.r,2*m.r);
+        F0R(i,m.r) F0R(j,m.r) x.d[i][j] = m.d[i][j];
+        F0R(i,m.r) x.d[i][i+m.r] = 1;
 
         if (gauss(x) == 0) return Mat<T>(0,0);
 
-        Mat<T> r(m.a,m.a);
-        F0R(i,m.a) F0R(j,m.a) r.d[i][j] = x.d[i][j+m.a];
+        Mat<T> r(m.r,m.r);
+        F0R(i,m.r) F0R(j,m.r) r.d[i][j] = x.d[i][j+m.r];
         return r;
     }
 }
--- Implementations/11 - Math (4)/11.3 - Matrix/Matrix.cpp
+++ Implementations/11 - Math (4)/11.3 - Matrix/Matrix.cpp
@@ -5,56 +5,37 @@
  */
 
 template<class T> struct Mat {
-    T** d;
-    int a, b;
+    int r,c;
+    vector<vector<T>> d;
+    Mat(int _r, int _c) : r(_r), c(_c) { d.assign(r,vector<T>(c)); }
+    Mat() : Mat(0,0) {} 
+    Mat (const vector<vector<T>>& _d) : r(sz(_d)), c(sz(_d[0])) { d = _d; }
+    friend void pr(const Mat& m) { pr(m.d); }
     
-    Mat() { a = b = 0; }
-    Mat(int _a, int _b) : a(_a), b(_b) {
-        d = new T*[a];
-        F0R(i,a) {
-            d[i] = new T[b];
-            F0R(j,b) d[i][j] = 0;
-        }
+    Mat& operator+=(const Mat& m) {
+        assert(r == m.r && c == m.c);
+        F0R(i,r) F0R(j,c) d[i][j] += m.d[i][j];
+        return *this;
     }
-    Mat (const vector<vector<T>>& v) : Mat(sz(v),sz(v[0])) {
-        F0R(i,a) F0R(j,b) d[i][j] = v[i][j];
-    }
-
-    operator vector<vector<T>> () {
-        auto ret = vector<vector<T>>(a,vector<T>(b));
-        F0R(i,a) F0R(j,b) ret[i][j] = d[i][j];
-        return ret;
-    }
-    friend void pr(const Mat& m) {
-        Mat M(m); pr(vector<vector<T>>(M));
-    }
-    
-    Mat operator+(const Mat& m) {
-        assert(a == m.a && b == m.b);
-        Mat r(a,b);
-        F0R(i,a) F0R(j,b) r.d[i][j] = d[i][j]+m.d[i][j];
-        return r;
-    }
-    Mat operator-(const Mat& m) {
-        assert(a == m.a && b == m.b);
-        Mat r(a,b);
-        F0R(i,a) F0R(j,b) r.d[i][j] = d[i][j]-m.d[i][j];
-        return r;
+    Mat& operator-=(const Mat& m) {
+        assert(r == m.r && c == m.c);
+        F0R(i,r) F0R(j,c) d[i][j] -= m.d[i][j];
+        return *this;
     }
     Mat operator*(const Mat& m) {
-        assert(b == m.a);
-        Mat r(a,m.b);
-        F0R(i,a) F0R(j,b) F0R(k,m.b) r.d[i][k] += d[i][j]*m.d[j][k];
-        return r;
+        assert(c == m.r); Mat x(r,m.c);
+        F0R(i,r) F0R(j,c) F0R(k,m.c) x.d[i][k] += d[i][j]*m.d[j][k];
+        return x;
     }
     
-    Mat& operator+=(const Mat& m) { return *this = (*this)+m; }
-    Mat& operator-=(const Mat& m) { return *this = (*this)-m; }
+    Mat operator+(const Mat& m) { return Mat(*this)+=m; }
+    Mat operator-(const Mat& m) { return Mat(*this)-=m; }
     Mat& operator*=(const Mat& m) { return *this = (*this)*m; }
 
     friend Mat exp(Mat m, ll p) {
-        Mat r(m.a,m.a); assert(m.a == m.b);
-        F0R(i,m.a) r.d[i][i] = 1;
+        assert(m.r == m.c);
+        Mat r(m.r,m.c);
+        F0R(i,m.r) r.d[i][i] = 1;
         for (; p; p /= 2, m *= m) if (p&1) r *= m;
         return r;
     }

ee15a57339e206627b8cb8653b97b227bfdbe805
Benjamin Qi <bqi343@gmail.com>
2019-06-22 15:51:16 -0400

    Update Line Container.cpp

--- Implementations/13 - Geometry (4)/13.2 - Convexity/Line Container.cpp
+++ Implementations/13 - Geometry (4)/13.2 - Convexity/Line Container.cpp
@@ -3,7 +3,7 @@
  * Source: KACTL
  * Verification: 
    * CSA Squared Ends
-   * https://codeforces.com/contest/1179/problem/D
+   * https://atcoder.jp/contests/arc066/tasks/arc066_d
  */
 
 bool Q;
@@ -32,6 +32,7 @@ struct LC : multiset<Line> {
 		if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
 		while ((y = x) != begin() && (--x)->p >= y->p) isect(x, erase(y));
 	}
+	
 	ll query(ll x) { 
 		assert(!empty());
 		Q = 1; auto l = *lb({0,0,x}); Q = 0;
@@ -47,25 +48,37 @@ struct LC0 : deque<Line> { // linear time w/ additional assumptions
 		if (x.k == y.k) return x.m >= y.m ? inf : -inf;
 		return div(y.m-x.m,x.k-y.k);
 	}
-	bool isect(iterator x, iterator y) { // updates x->p, determines if y is unneeded
-		if (y == end()) { x->p = inf; return 0; }
-		x->p = bet(*x,*y); return x->p >= y->p;
+	
+	void addBack(Line L) { // assume nonempty
+		while (1) {
+			auto a = back(); pop_back(); a.p = bet(a,L);
+			if (size() && back().p >= a.p) continue;
+			pb(a); break;
+		}
+		L.p = inf; pb(L);
+	}
+	void addFront(Line L) {
+		while (1) {
+			if (!size()) { L.p = inf; break; }
+			if ((L.p = bet(L,front())) >= front().p) pop_front();
+			else break;
+		}
+		push_front(L);
+	}
+	void add(ll k, ll m) { // assume line goes to one end of deque
+		if (!size() || k <= front().k) addFront({k,m,0});
+		else assert(k >= back().k), addBack({k,m,0});
 	}
 	
-	void add(ll k, ll m) { // assume line goes to one end of deque
-		if (!size() || k >= back().k) {
-			push_back({k,m,0});
-			auto z = prev(end()), y = z++, x = y;
-			while ((y = x) != begin() && (--x)->p >= y->p) isect(x, erase(y));
-		} else if (k <= front().k) {
-			push_front({k,m,0});
-			auto z = begin(), y = z++, x = y;
-			while (isect(y,z)) z = erase(z);
-		} else assert(0);
-	}
-	ll query(ll x) { // assume x comes in increasing order
-		assert(!empty());
-		while (front().p < x) pop_front();
-		return front().eval(x);
-	}
+	int ord = 0; // 1 = increasing, -1 = decreasing
+	ll query(ll x) { 
+		assert(ord);
+		if (ord == 1) { 
+			while (front().p < x) pop_front();
+			return front().eval(x);
+		} else {
+			while (size() > 1 && prev(prev(end()))->p >= x) pop_back();
+			return back().eval(x);
+		}
+	}	
 };

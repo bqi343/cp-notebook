15581cac9e9ed1f66d1e9dbb4777d4917048f53a
Benq <bqi343@gmail.com>
2019-06-25 22:32:00 -0400

    minor

--- Implementations/06 - DP (3)/Longest Increasing Subsequence.cpp
+++ Implementations/06 - DP (3)/Longest Increasing Subsequence.cpp
@@ -7,7 +7,7 @@
 vi bes = {INT_MIN}; // min last term of increasing sequence with i terms
 
 void add(int x) { // add terms of sequence one by one
-    int lo = lb(all(bes),x)-beg(bes);
+    int lo = lb(all(bes),x)-begin(bes);
     if (lo == sz(bes)) bes.pb(0);
     bes[lo] = x; // sz(bes)-1 is your current answer
 }
--- Implementations/11 - Math (4)/Number Theory Advanced/Order (6).cpp
+++ Implementations/11 - Math (4)/Number Theory Advanced/Order (6).cpp
@@ -1,7 +1,7 @@
 /**
  * Description: Calculates smallest P such that x^P equiv 1 (mod p)
  * Source: Own
- * Verification: ?
+ * Verification: https://atcoder.jp/contests/jag2015summer-day4/tasks/icpc2015summer_day4_d
  */
 
 using namespace rpm;
@@ -9,7 +9,7 @@ using namespace basicFactor;
 
 ll order(ll x, ll p) {
     if (__gcd(x,p) != 1) return 0;
-    ll P = phi(p); auto a = fac(P);
+    ll P = phi(p); auto a = factor(P);
     trav(t,a) while (P % t.f == 0 && mod_pow(x,P/t.f,p) == 1) P /= t.f;
     return P;
 }

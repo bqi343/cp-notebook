2d08c253e0aa735fd536e0144d0fbf8d29883395
Benq <bqi343@gmail.com>
2019-07-17 19:19:10 -0400

    Update Aho-Corasick.cpp

--- Implementations/14 - Strings (3)/14.3 - Z (4)/Aho-Corasick.cpp
+++ Implementations/14 - Strings (3)/14.3 - Z (4)/Aho-Corasick.cpp
@@ -5,27 +5,30 @@
  */
 
 template<int SZ> struct AhoCorasick {
-    int link[SZ], dict[SZ], sz = 1, num = 0;
-    vpi ind[SZ];
+    int sz = 1, num = 0; // sz = # of nodes, num = # strings added
+    int link[SZ]; // longest suffix
     map<char,int> to[SZ];
-    vi oc[SZ];
     queue<int> q;
+    
+    int word[SZ]; // longest suffix which is a word
+    vpi words[SZ]; // store word(s) at trie position
+    vi oc[SZ]; // store occurrences of each word
 
     AhoCorasick() {
         memset(link,0,sizeof link);
-        memset(dict,0,sizeof dict);
+        memset(word,0,sizeof word);
     }
 
-    void add(string s) {
+    void add(string s) { // add word
         int v = 0;
         trav(c,s) {
             if (!to[v].count(c)) to[v][c] = sz++;
             v = to[v][c];
         }
-        dict[v] = v; ind[v].pb({++num,sz(s)});
+        word[v] = v; words[v].pb({++num,sz(s)});
     }
 
-    void pushLinks() {
+    void init() { // generate links
         link[0] = -1; q.push(0);
         while (sz(q)) {
             int v = q.front(); q.pop();
@@ -34,7 +37,7 @@ template<int SZ> struct AhoCorasick {
                 while (j != -1 && !to[j].count(c)) j = link[j];
                 if (j != -1) {
                     link[u] = to[j][c];
-                    if (!dict[u]) dict[u] = dict[link[u]];
+                    if (!word[u]) word[u] = word[link[u]];
                 }
                 q.push(u);
             }
@@ -42,16 +45,16 @@ template<int SZ> struct AhoCorasick {
     }
 
     void process(int pos, int cur) { // process matches
-        cur = dict[cur];
+        cur = word[cur];
         while (cur) {
-            trav(a,ind[cur]) oc[a.f].pb(pos-a.s+1);
-            cur = dict[link[cur]];
+            trav(a,words[cur]) oc[a.f].pb(pos-a.s+1);
+            cur = word[link[cur]];
         }
     }
 
     int nex(int pos, int cur, char c) {
         // get position after adding character
-        // speed up with memoization
+        // if alphabet size is limited, speed up with memoization 
         while (cur != -1 && !to[cur].count(c)) cur = link[cur];
         if (cur == -1) cur = 0;
         else cur = to[cur][c];

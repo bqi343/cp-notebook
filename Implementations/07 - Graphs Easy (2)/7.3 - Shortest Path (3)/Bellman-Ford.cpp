93acb50230d143492066f207ee357de791664f7a
Benjamin Qi <bqi343@gmail.com>
2019-08-26 22:24:09 -0400

    Update Bellman-Ford.cpp

--- Implementations/07 - Graphs Easy (2)/7.3 - Shortest Path (3)/Bellman-Ford.cpp
+++ Implementations/07 - Graphs Easy (2)/7.3 - Shortest Path (3)/Bellman-Ford.cpp
@@ -8,24 +8,28 @@
 
 template<int SZ> struct BellmanFord {
     int n;
+    vi adj[SZ];
     vector<pair<pi,int>> ed;
-    void addEdge(int u, int v, int w) { ed.pb({{u,v},w}); }
+    void addEdge(int u, int v, int w) { 
+        adj[u].pb(v), ed.pb({{u,v},w}); 
+    }
     
-    bool bad[SZ];
     ll dist[SZ];
-    ll getDist(int x) { return bad[x] ? -INF : dist[x]; }
-
+    void genBad(int x) { 
+        // if x is reachable from negative cycle
+        // -> update dists of all vertices which x can go to
+        if (dist[x] == -INF) return;
+        dist[x] = -INF; 
+        trav(t,adj[x]) genBad(t);
+    }
     void init(int _n, int s) {
-        n = _n;
-        FOR(i,1,n+1) dist[i] = INF, bad[i] = 0;
+        n = _n; FOR(i,1,n+1) dist[i] = INF; // one-indexed
         dist[s] = 0;
-
+        
         F0R(i,n) trav(a,ed) if (dist[a.f.f] < INF)
-            dist[a.f.s] = min(dist[a.f.s], dist[a.f.f]+a.s);
-
-        trav(a,ed) if (dist[a.f.f] < INF)
-            if (dist[a.f.s] > dist[a.f.f]+a.s) bad[a.f.s] = 1;
-
-        F0R(i,n) trav(a,ed) if (bad[a.f.f]) bad[a.f.s] = 1;
+            ckmin(dist[a.f.s],dist[a.f.f]+a.s);
+        trav(a,ed) if (dist[a.f.f] < INF 
+                    && dist[a.f.s] > dist[a.f.f]+a.s) 
+            genBad(a.f.s);
     }
 };

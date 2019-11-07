5a3d3e2a0d6dd9f794413715555683b39234fb10
Benjamin Qi <bqi343@gmail.com>
2019-11-07 16:45:31 -0500

    hash

---
+++ Implementations/content/strings (14)/Light/HashRange (14.2).h
@@ -0,0 +1,45 @@
+/**
+ * Description: polynomial double-hash substrings
+ * Usage: HashRange H; H.init("ababab"); F0R(i,6) FOR(j,i,6) ps(i,j,H.hash(i,j));
+ * Source:
+	* KACTL
+	* https://codeforces.com/contest/1207/submission/59309672
+ * Verification: 
+	* USACO Dec 17 Plat 1
+	* CF Check Transcription
+ */
+
+typedef array<int,2> T;
+// not too close to ends
+uniform_int_distribution<int> MULT_DIST(0.1*MOD,0.9*MOD); 
+const T base = {MULT_DIST(rng),MULT_DIST(rng)};
+
+T operator+(const T& l, const T& r) { T x; 
+	F0R(i,2) x[i] = (l[i]+r[i])%MOD; return x; }
+T operator-(const T& l, const T& r) { T x; 
+	F0R(i,2) x[i] = (l[i]-r[i]+MOD)%MOD; return x; }
+T operator*(const T& l, const T& r) { T x; 
+	F0R(i,2) x[i] = (ll)l[i]*r[i]%MOD; return x; }
+
+struct HashRange {
+	string S; 
+	vector<T> pows, cum;
+	void init(string _S) {
+		S = _S; pows.rsz(sz(S)), cum.rsz(sz(S)+1);
+		pows[0] = {1,1}; FOR(i,1,sz(S)) pows[i] = pows[i-1]*base;
+		F0R(i,sz(S)) {
+			int c = S[i]-'a'+1;
+			cum[i+1] = base*cum[i]+T{c,c};
+		}
+	}
+	T hash(int l, int r) { return cum[r+1]-pows[r+1-l]*cum[l]; }
+	int lcp(HashRange& b) {
+		int lo = 0, hi = min(sz(S),sz(b.S));
+		while (lo < hi) {
+			int mid = (lo+hi+1)/2;
+			if (cum[mid] == b.cum[mid]) lo = mid;
+			else hi = mid-1;
+		}
+		return lo;
+	}
+};
--- Implementations/content/strings (14)/Light/PairOp.h
+++ Implementations/content/strings (14)/Light/PairOp.h
@@ -1,14 +1,8 @@
 /**
- * Description: polynomial double-hash substrings
- * Sources:
-	* KACTL
-	* https://codeforces.com/contest/1207/submission/59309672
- * Verification: 
-	* USACO Dec 17 Plat 1
-	* CF Check Transcription
+ * Description: operations with pairs
+ * Source: own
+ * Verification: see HashRange
  */
-
-#include "../../number-theory (11.1)/Modular Arithmetic/Modular.h"
 
 namespace pairOp {
 	template<class A, class B> pair<A,B> operator+(const pair<A,B>& l, const pair<A,B>& r) { return {l.f+r.f,l.s+r.s}; }
@@ -26,34 +20,3 @@ namespace pairOp {
 }
 
 using namespace pairOp;
-
-uniform_int_distribution<int> MULT_DIST(0.1 * MOD, 0.9 * MOD); // not too close to ends
-const pmi base = {MULT_DIST(rng),MULT_DIST(rng)};
-
-struct hsh {
-	string S; 
-	vpmi pows, cum;
-	
-	void init(string _S) {
-		S = _S; pows.rsz(sz(S)), cum.rsz(sz(S)+1);
-		pows[0] = {1,1}; FOR(i,1,sz(S)) pows[i] = pows[i-1]*base;
-		F0R(i,sz(S)) {
-			int c = S[i]-'a'+1;
-			cum[i+1] = base*cum[i]+pmi(c,c);
-		}
-	}
-	
-	pmi get(int l, int r) {	 return cum[r+1]-pows[r+1-l]*cum[l]; }
-	
-	int lcp(hsh& b) {
-		int lo = 0, hi = min(sz(S),sz(b.S));
-		while (lo < hi) {
-			int mid = (lo+hi+1)/2;
-			if (cum[mid] == b.cum[mid]) lo = mid;
-			else hi = mid-1;
-		}
-		return lo;
-	}
-};
-
-// hsh H; H.init("ababab"); F0R(i,6) FOR(j,i,6) ps(i,j,H.get(i,j));

9118fedec149f2ecf8a658bdc41c5e4fb0126ab1
Benjamin Qi <bqi343@gmail.com>
2020-12-23 19:45:22 -0500

    + genomics slow

--- Contests/Tools/USACO/usaco_gen.cpp
+++ Contests/Tools/USACO/usaco_gen.cpp
@@ -3,60 +3,130 @@
  * Source: Own + Dhruv Rohatgi
  */
 
-int RNG(int L, int R) { assert(L <= R); // int in [L,R] inclusive
-	return uniform_int_distribution<int>(L,R)(rng);  }
-db RNG(db L, db R) { assert(L <= R);
-	return uniform_real_distribution<db>(L,R)(rng); }
-int geoRNG(db p) { assert(0 < p && p <= 1); // p large -> closer to 0
-	int c = 0; while (RNG(0.0,1.0) > p) c ++; // mean 1/p-1
-	return c; }
-template<class T> void shuf(vector<T>& v) { shuffle(all(v),rng); }
-vi randPerm(int N) { vi v(N); iota(all(v),0);
-	shuf(v); return v; }
+////////////// DISTRIBUTIONS
 
-vpi shufTree(vpi ed) { // randomly swap endpoints, rearrange labels
-	int N = sz(ed)+1; vi v = randPerm(N);
+// return int in [L,R] inclusive
+int rng_int(int L, int R) { assert(L <= R); 
+	return uniform_int_distribution<int>(L,R)(rng);  }
+
+// return double in [L,R] inclusive
+db rng_db(db L, db R) { assert(L <= R);
+	return uniform_real_distribution<db>(L,R)(rng); }
+
+// http://cplusplus.com/reference/random/geometric_distribution/geometric_distribution/
+// flip a coin which is heads with probability p until you flip heads
+// mean value of c is 1/p-1
+int rng_geo(db p) { assert(0 < p && p <= 1); // p large -> closer to 0
+	return geometric_distribution<int>(p)(rng); }
+
+////////////// VECTORS + PERMS
+
+// shuffle a vector
+template<class T> void shuf(vector<T>& v) { shuffle(all(v),rng); }
+
+// generate random permutation of [0,N-1]
+vi randPerm(int N) { vi v(N); iota(all(v),0); shuf(v); return v; }
+
+// random permutation of [0,N-1] with first element 0
+vi randPermZero(int N) { vi v(N-1); iota(all(v),1);
+	shuf(v); v.ins(bg(v),0); return v; }
+
+// shuffle permutation of [0,N-1]
+vi shufPerm(vi v) {
+	int N = sz(v); vi key = randPerm(N);
+	vi res(N); F0R(i,N) res[key[i]] = key[v[i]];
+	return res;
+}
+
+// vector with all entries in [L,R]
+vi rng_vec(int N, int L, int R) {
+	vi res; F0R(_,N) res.pb(rng_int(L,R));
+	return res;
+}
+
+// vector with all entries in [L,R], unique
+vi rng_vec_unique(int N, int L, int R) {
+	set<int> so_far; vi res;
+	F0R(_,N) {
+		int x; do { x = rng_int(L,R); } while (so_far.count(x));
+		so_far.ins(x); res.pb(x);
+	}
+	return res;
+}
+
+////////////// GRAPHS
+
+// relabel edges ed according to perm, shuffle
+vpi relabelAndShuffle(vpi ed, vi perm) {
 	trav(t,ed) {
-		t.f = v[t.f], t.s = v[t.s];
+		t.f = perm[t.f], t.s = perm[t.s];
 		if (rng()&1) swap(t.f,t.s);
 	}
 	shuf(ed); return ed;
 }
-vpi randTree(int N, int back) { // smaller back -> closer to line
-	assert(N >= 1 && back >= 0); vpi ed; 
-	FOR(i,1,N) ed.eb(i,i-1-RNG(0,min(back,i-1)));
-	return ed; }
-vpi path(int N) { return randTree(N,0); }
 
-pi geoEdge(int i, db p) { assert(i > 0); // for tall tree
-	return {i,max(0,i-1-geoRNG(p))}; }
-vpi tallTree(int N, db p) {
-	assert(N >= 1); 
+// shuffle graph with vertices [0,N-1]
+vpi shufGraph(int N, vpi ed) { // randomly swap endpoints, rearrange labels
+	return relabelAndShuffle(ed,randPerm(N)); }
+
+// shuffle tree given N-1 edges
+vpi shufTree(vpi ed) { return shufGraph(sz(ed)+1,ed); }
+
+// randomly swap endpoints, rearrange labels
+vpi shufRootedTree(vpi ed) {
+	return relabelAndShuffle(ed,randPermZero(sz(ed)+1)); }
+
+////////////// GENERATING TREES
+
+// for generating tall tree
+pi geoEdge(int i, db p) { assert(i > 0); 
+	return {i,max(0,i-1-rng_geo(p))}; }
+
+// generate edges of tree with verts [0,N-1]
+// smaller back -> taller tree
+vpi treeRand(int N, int back) { 
+	assert(N >= 1 && back >= 0); vpi ed; 
+	FOR(i,1,N) ed.eb(i,i-1-rng_int(0,min(back,i-1)));
+	return ed; }
+
+// generate path
+vpi path(int N) { return treeRand(N,0); }
+
+// generate tall tree (large diameter)
+// the higher the p the taller the tree
+vpi treeTall(int N, db p) { assert(N >= 1); 
 	vpi ed; FOR(i,1,N) ed.pb(geoEdge(i,p));
 	return ed; }
-vpi randTallTree(int N, db p) { // tall tree, then add rand 
+
+// generate tall tree, then add rand at end
+vpi treeTallShort(int N, db p) { 
 	assert(N >= 1); int mid = (N+1)/2;
-	vpi ed = tallTree(mid,p); 
-	FOR(i,mid,N) ed.eb(i,RNG(0,i-1));
+	vpi ed = treeTall(mid,p); 
+	FOR(i,mid,N) ed.eb(i,rng_int(0,i-1));
 	return ed; }
-vpi heavyTallTree(int N, db p) { // lots of stuff connected to heavy1 or heavy2
+
+// lots of stuff connected to either heavy1 or heavy2
+vpi treeTallHeavy(int N, db p) { 
 	assert(N >= 1); // + bunch of rand
 	vpi ed; int heavy1 = 0, heavy2 = N/2;
 	FOR(i,1,N) {
 		if(i < N/4) ed.eb(i,heavy1);
 		else if (i > heavy2 && i < 3*N/4) ed.eb(i,heavy2);
 		else ed.pb(geoEdge(i,p));
-	}	
+	}
 	return ed;
 }
-vpi heavyTallShortTree(int N, db p) { // connected to heavy1 or heavy2
+
+// heavy tall tree + random
+// lots of verts connected to heavy1 or heavy2
+vpi treeTallHeavyShort(int N, db p) { 
 	assert(N >= 1); // + almost-path + rand
 	vpi ed; int heavy1 = 0, heavy2 = N/2;
 	FOR(i,1,N) {
 		if(i < N/4) ed.eb(i,heavy1);
 		else if (i <= heavy2) ed.pb(geoEdge(i,p)); // tall -> heavy1
 		else if (i > heavy2 && i < 3*N/4) ed.eb(i,heavy2);
-		else ed.eb(i,RNG(0,i-1)); // random stuff
+		else ed.eb(i,rng_int(0,i-1));
 	}
 	return ed;
 }
---
+++ Contests/USACO Solutions/2020-21/Dec/Gold/genomics_N2.cpp
@@ -0,0 +1,308 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+using ld = long double;
+using db = double; 
+using str = string; // yay python!
+ 
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using pd = pair<db,db>;
+ 
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<db>; 
+using vs = vector<str>;
+using vpi = vector<pi>;
+using vpl = vector<pl>; 
+using vpd = vector<pd>;
+ 
+#define tcT template<class T
+#define tcTU tcT, class U
+// ^ lol this makes everything look weird but I'll try it
+tcT> using V = vector<T>; 
+tcT, size_t SZ> using AR = array<T,SZ>; 
+tcT> using PR = pair<T,T>;
+ 
+// pairs
+#define mp make_pair
+#define f first
+#define s second
+ 
+// vectors
+// oops size(x), rbegin(x), rend(x) need C++17
+#define sz(x) int((x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend() 
+#define sor(x) sort(all(x)) 
+#define rsz resize
+#define ins insert 
+#define ft front()
+#define bk back()
+#define pb push_back
+#define eb emplace_back 
+#define pf push_front
+ 
+#define lb lower_bound
+#define ub upper_bound 
+tcT> int lwb(V<T>& a, const T& b) { return int(lb(all(a),b)-bg(a)); }
+ 
+// loops
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a: x)
+ 
+const int MOD = 1e9+7; // 998244353;
+const int MX = 2e5+5;
+const ll INF = 1e18; // not too close to LLONG_MAX
+const ld PI = acos((ld)-1);
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1}; // for every grid problem!!
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count()); 
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>;
+ 
+// bitwise ops
+// also see https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) { // assert(x >= 0); // make C++11 compatible until USACO updates ...
+	return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+ 
+ll cdiv(ll a, ll b) { return a/b+((a^b)>0&&a%b); } // divide a by b rounded up
+ll fdiv(ll a, ll b) { return a/b-((a^b)<0&&a%b); } // divide a by b rounded down
+ 
+tcT> bool ckmin(T& a, const T& b) {
+	return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+tcT> bool ckmax(T& a, const T& b) {
+	return a < b ? a = b, 1 : 0; }
+ 
+tcTU> T fstTrue(T lo, T hi, U f) {
+	hi ++; assert(lo <= hi); // assuming f is increasing
+	while (lo < hi) { // find first index such that f is true 
+		T mid = lo+(hi-lo)/2;
+		f(mid) ? hi = mid : lo = mid+1; 
+	} 
+	return lo;
+}
+tcTU> T lstTrue(T lo, T hi, U f) {
+	lo --; assert(lo <= hi); // assuming f is decreasing
+	while (lo < hi) { // find first index such that f is true 
+		T mid = lo+(hi-lo+1)/2;
+		f(mid) ? lo = mid : hi = mid-1;
+	} 
+	return lo;
+}
+tcT> void remDup(vector<T>& v) { // sort and remove duplicates
+	sort(all(v)); v.erase(unique(all(v)),end(v)); }
+tcTU> void erase(T& t, const U& u) { // don't erase
+	auto it = t.find(u); assert(it != end(t));
+	t.erase(it); } // element that doesn't exist from (multi)set
+ 
+// INPUT
+#define tcTUU tcT, class ...U
+tcT> void re(complex<T>& c);
+tcTU> void re(pair<T,U>& p);
+tcT> void re(V<T>& v);
+tcT, size_t SZ> void re(AR<T,SZ>& a);
+ 
+tcT> void re(T& x) { cin >> x; }
+void re(db& d) { str t; re(t); d = stod(t); }
+void re(ld& d) { str t; re(t); d = stold(t); }
+tcTUU> void re(T& t, U&... u) { re(t); re(u...); }
+ 
+tcT> void re(complex<T>& c) { T a,b; re(a,b); c = {a,b}; }
+tcTU> void re(pair<T,U>& p) { re(p.f,p.s); }
+tcT> void re(V<T>& x) { trav(a,x) re(a); }
+tcT, size_t SZ> void re(AR<T,SZ>& x) { trav(a,x) re(a); }
+tcT> void rv(int n, V<T>& x) { x.rsz(n); re(x); }
+ 
+// TO_STRING
+#define ts to_string
+str ts(char c) { return str(1,c); }
+str ts(const char* s) { return (str)s; }
+str ts(str s) { return s; }
+str ts(bool b) { 
+	#ifdef LOCAL
+		return b ? "true" : "false"; 
+	#else 
+		return ts((int)b);
+	#endif
+}
+tcT> str ts(complex<T> c) { 
+	stringstream ss; ss << c; return ss.str(); }
+str ts(V<bool> v) {
+	str res = "{"; F0R(i,sz(v)) res += char('0'+v[i]);
+	res += "}"; return res; }
+template<size_t SZ> str ts(bitset<SZ> b) {
+	str res = ""; F0R(i,SZ) res += char('0'+b[i]);
+	return res; }
+tcTU> str ts(pair<T,U> p);
+tcT> str ts(T v) { // containers with begin(), end()
+	#ifdef LOCAL
+		bool fst = 1; str res = "{";
+		for (const auto& x: v) {
+			if (!fst) res += ", ";
+			fst = 0; res += ts(x);
+		}
+		res += "}"; return res;
+	#else
+		bool fst = 1; str res = "";
+		for (const auto& x: v) {
+			if (!fst) res += " ";
+			fst = 0; res += ts(x);
+		}
+		return res;
+ 
+	#endif
+}
+tcTU> str ts(pair<T,U> p) {
+	#ifdef LOCAL
+		return "("+ts(p.f)+", "+ts(p.s)+")"; 
+	#else
+		return ts(p.f)+" "+ts(p.s);
+	#endif
+}
+ 
+// OUTPUT
+tcT> void pr(T x) { cout << ts(x); }
+tcTUU> void pr(const T& t, const U&... u) { 
+	pr(t); pr(u...); }
+void ps() { pr("\n"); } // print w/ spaces
+tcTUU> void ps(const T& t, const U&... u) { 
+	pr(t); if (sizeof...(u)) pr(" "); ps(u...); }
+ 
+// DEBUG
+void DBG() { cerr << "]" << endl; }
+tcTUU> void DBG(const T& t, const U&... u) {
+	cerr << ts(t); if (sizeof...(u)) cerr << ", ";
+	DBG(u...); }
+#ifdef LOCAL // compile with -DLOCAL, chk -> fake assert
+	#define dbg(...) cerr << "Line(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+	#define chk(...) if (!(__VA_ARGS__)) cerr << "Line(" << __LINE__ << ") -> function(" \
+		 << __FUNCTION__  << ") -> CHK FAILED: (" << #__VA_ARGS__ << ")" << "\n", exit(0);
+#else
+	#define dbg(...) 0
+	#define chk(...) 0
+#endif
+ 
+void setPrec() { cout << fixed << setprecision(15); }
+void unsyncIO() { cin.tie(0)->sync_with_stdio(0); }
+// FILE I/O
+void setIn(str s) { freopen(s.c_str(),"r",stdin); }
+void setOut(str s) { freopen(s.c_str(),"w",stdout); }
+void setIO(str s = "") {
+	unsyncIO(); setPrec();
+	// cin.exceptions(cin.failbit); 
+	// throws exception when do smth illegal
+	// ex. try to read letter into int
+	if (sz(s)) setIn(s+".in"), setOut(s+".out"); // for USACO
+}
+ 
+/**
+ * Description: modular arithmetic operations 
+ * Source: 
+	* KACTL
+	* https://codeforces.com/blog/entry/63903
+	* https://codeforces.com/contest/1261/submission/65632855 (tourist)
+	* https://codeforces.com/contest/1264/submission/66344993 (ksun)
+	* also see https://github.com/ecnerwala/cp-book/blob/master/src/modnum.hpp (ecnerwal)
+ * Verification: 
+	* https://open.kattis.com/problems/modulararithmetic
+ */
+ 
+template<int MOD, int RT> struct mint {
+	static const int mod = MOD;
+	static constexpr mint rt() { return RT; } // primitive root for FFT
+	int v; explicit operator int() const { return v; } // explicit -> don't silently convert to int
+	mint() { v = 0; }
+	mint(ll _v) { v = int((-MOD < _v && _v < MOD) ? _v : _v % MOD);
+		if (v < 0) v += MOD; }
+	friend bool operator==(const mint& a, const mint& b) { 
+		return a.v == b.v; }
+	friend bool operator!=(const mint& a, const mint& b) { 
+		return !(a == b); }
+	friend bool operator<(const mint& a, const mint& b) { 
+		return a.v < b.v; }
+	friend void re(mint& a) { ll x; re(x); a = mint(x); }
+	friend str ts(mint a) { return ts(a.v); }
+   
+	mint& operator+=(const mint& m) { 
+		if ((v += m.v) >= MOD) v -= MOD; 
+		return *this; }
+	mint& operator-=(const mint& m) { 
+		if ((v -= m.v) < 0) v += MOD; 
+		return *this; }
+	mint& operator*=(const mint& m) { 
+		v = (ll)v*m.v%MOD; return *this; }
+	mint& operator/=(const mint& m) { return (*this) *= inv(m); }
+	friend mint pow(mint a, ll p) {
+		mint ans = 1; assert(p >= 0);
+		for (; p; p /= 2, a *= a) if (p&1) ans *= a;
+		return ans; }
+	friend mint inv(const mint& a) { assert(a.v != 0); 
+		return pow(a,MOD-2); }
+		
+	mint operator-() const { return mint(-v); }
+	mint& operator++() { return *this += 1; }
+	mint& operator--() { return *this -= 1; }
+	friend mint operator+(mint a, const mint& b) { return a += b; }
+	friend mint operator-(mint a, const mint& b) { return a -= b; }
+	friend mint operator*(mint a, const mint& b) { return a *= b; }
+	friend mint operator/(mint a, const mint& b) { return a /= b; }
+};
+ 
+typedef mint<MOD,5> mi; // 5 is primitive root for both common mods
+typedef vector<mi> vmi;
+typedef pair<mi,mi> pmi;
+typedef vector<pmi> vpmi;
+ 
+vector<vmi> scmb; // small combinations
+void genComb(int SZ) {
+	scmb.assign(SZ,vmi(SZ)); scmb[0][0] = 1;
+	FOR(i,1,SZ) F0R(j,i+1) 
+		scmb[i][j] = scmb[i-1][j]+(j?scmb[i-1][j-1]:0);
+}
+ 
+str S, AGCT = "AGCT";
+mi dp[100001][4];
+ 
+int main() {
+	setIO(); re(S);
+	F0R(i,4) dp[0][i] = 1;
+	int N = sz(S);
+	F0R(l,N) F0R(a,4) { // last string so far begins with a
+		if (dp[l][a] == 0) continue;
+		F0R(b,4) {
+			auto red = [&](int ind, vmi& v) {
+				char c = S[ind]; if (c == '?') return;
+				F0R(i,4) if (AGCT[i] != c) v[i] = 0;
+			};
+			vmi tmp(4); tmp[b] = dp[l][a]; 
+			red(l,tmp); 
+			FOR(r,l+1,N+1) {
+				dp[r][b] += tmp[a];
+				if (r < N) {
+					vmi TMP(4); F0R(x,4) F0R(y,4) if (x != y) TMP[y] += tmp[x];
+					red(r,TMP);
+					tmp = TMP;
+				}
+			}
+		}
+	}
+	mi ans = 0;
+	F0R(i,4) ans += dp[N][i];
+	ps(ans);
+}
+ 
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/

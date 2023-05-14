08e3d0d722584442bd38280251d5e65f7ac61612
Benjamin Qi <bqi343@gmail.com>
2023-05-14 15:47:53 -0400

    remove old Dinic / MCMF

--- Implementations/content/graphs (12)/Flows (12.3)/Dinic.h
+++ Implementations/content/graphs (12)/Flows (12.3)/Dinic.h
@@ -1,46 +1,42 @@
 /**
  * Description: Fast flow. After computing flow, edges $\{u,v\}$ such that 
-	* $lev[u] \neq 0,$ $lev[v] = 0$ are part of min cut.
- * Time: $O(N^2M)$ flow
+	* $lev[u] \neq -1,$ $lev[v] = -1$ are part of min cut.
+	* Use \texttt{reset} and \texttt{rcap} for Gomory-Hu.
+ * Time: $O(N^2M)$ flow, $O(M\sqrt N)$ bipartite matching
  * Source: GeeksForGeeks, Chilli
- 	* https://codeforces.com/contest/1416/submission/94013395
  * Verification: RMI 2017 Day 1 Fashion
 	* https://pastebin.com/VJxTvEg1
  */
 
-template<class F> struct Dinic {
-	struct Edge { int to, rev; F cap; };
-	int N; V<V<Edge>> adj;
-	void init(int _N) { N = _N; adj.rsz(N); }
-	pi ae(int a, int b, F cap, F rcap = 0) { 
-		assert(min(cap,rcap) >= 0); // saved me > once
-		adj[a].pb({b,sz(adj[b]),cap});
-		adj[b].pb({a,sz(adj[a])-1,rcap});
-		return {a,sz(adj[a])-1};
+struct Dinic {
+	using F = ll; // flow type
+	struct Edge { int to; F flo, cap; };
+	int N; V<Edge> eds; V<vi> adj;
+	void init(int _N) { N = _N; adj.rsz(N), cur.rsz(N); }
+	/// void reset() { each(e,eds) e.flo = 0; }
+	void ae(int u, int v, F cap, F rcap = 0) { assert(min(cap,rcap) >= 0); 
+		adj[u].pb(sz(eds)); eds.pb({v,0,cap});
+		adj[v].pb(sz(eds)); eds.pb({u,0,rcap});
 	}
-	F edgeFlow(pi loc) { // get flow along original edge
-		const Edge& e = adj.at(loc.f).at(loc.s);
-		return adj.at(e.to).at(e.rev).cap;
-	}
-	vi lev, ptr;
-	bool bfs(int s, int t) { // level=shortest dist from source
-		lev = ptr = vi(N);
-		lev[s] = 1; queue<int> q({s});
+	vi lev; V<vi::iterator> cur;
+	bool bfs(int s, int t) { // level = shortest distance from source
+		lev = vi(N,-1); F0R(i,N) cur[i] = begin(adj[i]);
+		queue<int> q({s}); lev[s] = 0; 
 		while (sz(q)) { int u = q.ft; q.pop();
-			each(e,adj[u]) if (e.cap && !lev[e.to]) {
-				q.push(e.to), lev[e.to] = lev[u]+1;
-				if (e.to == t) return 1;
+			each(e,adj[u]) { const Edge& E = eds[e];
+				int v = E.to; if (lev[v] < 0 && E.flo < E.cap) 
+					q.push(v), lev[v] = lev[u]+1;
 			}
 		}
-		return 0;
+		return lev[t] >= 0;
 	}
 	F dfs(int v, int t, F flo) {
 		if (v == t) return flo;
-		for (int& i = ptr[v]; i < sz(adj[v]); i++) {
-			Edge& e = adj[v][i];
-			if (lev[e.to]!=lev[v]+1||!e.cap) continue;
-			if (F df = dfs(e.to,t,min(flo,e.cap))) { 
-				e.cap -= df; adj[e.to][e.rev].cap += df;
+		for (; cur[v] != end(adj[v]); cur[v]++) {
+			Edge& E = eds[*cur[v]];
+			if (lev[E.to]!=lev[v]+1||E.flo==E.cap) continue;
+			F df = dfs(E.to,t,min(flo,E.cap-E.flo));
+			if (df) { E.flo += df; eds[*cur[v]^1].flo -= df;
 				return df; } // saturated >=1 one edge
 		}
 		return 0;

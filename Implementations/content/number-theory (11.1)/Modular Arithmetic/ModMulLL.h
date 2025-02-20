f01dbc034d5e883644ac83d6d2ee65445a80f264
rqi3 <54645143+rqi3@users.noreply.github.com>
2025-02-20 01:52:56 -0500

    Update ModMulLL.h

--- Implementations/content/number-theory (11.1)/Modular Arithmetic/ModMulLL.h
+++ Implementations/content/number-theory (11.1)/Modular Arithmetic/ModMulLL.h
@@ -8,9 +8,12 @@
 
 /// using db = long double;
 using ul = uint64_t;
-ul modMul(ul a, ul b, const ul mod) {
-	ll ret = a*b-mod*(ul)((db)a*b/mod);
-	return ret+((ret<0)-(ret>=(ll)mod))*mod; }
+// ul modMul(ul a, ul b, const ul mod) {
+// 	ll ret = a*b-mod*(ul)((db)a*b/mod);
+// 	return ret+((ret<0)-(ret>=(ll)mod))*mod; }
+ul modMul(ul a, ul b, const ul mod){
+	return __int128(a) * __int128(b) % mod;
+}
 ul modPow(ul a, ul b, const ul mod) {
 	if (b == 0) return 1;
 	ul res = modPow(a,b/2,mod); res = modMul(res,res,mod);
